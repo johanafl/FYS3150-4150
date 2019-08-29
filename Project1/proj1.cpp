a189239fe7866e14540e3f39c31a6cb74b206ea4
Johan <johanafl@student.matnat.uio.no>
2019-08-29 16:53:58 +0200

    Began Gaussian elimination, delete not implemented yet, memory allocation not yet done!

--- Project1/proj1.cpp
+++ Project1/proj1.cpp
@@ -1,6 +1,35 @@
 
-
-int main() 
+void set_arrays(int n) 
 {
+    a      = new double[n-1];
+    b      = new double[n];
+    c      = new double[n-1];
+    f      = new double[n];
+    v      = new double[n];
+    f_prim = new double[n];
+    b_prim = new double[n];
+}
+
+void gauss_elim(int n) 
+{
+    //
+}
+
+/*
+~ArrayList() 
+{
+    delete[] a;
+    delete[] b;
+    delete[] c;
+    delete[] f;
+    delete[] v;
+    delete[] f_prim;
+    delete[] b_prim;
+}
+*/
+
+int main(int argc, char *argv[]) 
+{
+    n = argv[1];
     return 0;
 }
