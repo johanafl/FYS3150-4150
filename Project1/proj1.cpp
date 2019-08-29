122aeaa8d8004435e3e20a01bcaa301f9bc6f622
Johan <johanafl@student.matnat.uio.no>
2019-08-29 17:16:24 +0200

    Done with gaussian elimination (special solution), still not finished with delete or memo allo

--- Project1/proj1.cpp
+++ Project1/proj1.cpp
@@ -12,7 +12,20 @@ void set_arrays(int n)
 
 void gauss_elim(int n) 
 {
-    //
+    b_prim[0] = b[0];
+    for (int i=1; i<=n; i++)
+    {
+        b_prim[i+1] = b[i+1] - a[i]/b[i]*c[i];
+        f_prim[i+1] = b[i+1] - a[i]/b[i]*c[i];
+    }
+    for (int i=n; i>=1; i--)
+    {
+        f_prim[i-1] = f_prim[i-1] - c[i-1]/b_prim[i]*f_prim[i];
+    }
+    for (int i=0; i<=n; i++)
+    {
+        v[i] = f_prim[i]/b_prim[i];
+    }
 }
 
 /*
