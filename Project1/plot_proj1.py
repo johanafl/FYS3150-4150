f02138c61d2274c6fb5c133635213f2b3cb140cd
Mats Ola Sand <matsolasand@outlook.com>
2019-09-01 21:49:09 +0200

    Generalized the code, so that it can take text-arguments from the command line

--- Project1/plot_proj1.py
+++ Project1/plot_proj1.py
@@ -2,9 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-def plot(axis, n, exact=False):
-    values = np.loadtxt("Poisson_values_n_{:d}.txt".format(n), skiprows=1)
+def plot_func(axis, n, args=False, exact=False):
 
+    try:
+        if not args:
+            values = np.loadtxt("Poisson_values_n_{:d}.txt".format(n), skiprows=1)
+    except ValueError:
+        values = args
 
     x     = np.linspace(0, 1, n+2)
     u     = values[:, 0]
@@ -12,21 +16,38 @@ def plot(axis, n, exact=False):
     error = values[:, 2]
 
     axis[0].plot(x, v, "-", label="v, n={:d}".format(n))
-    axis[1].loglog(n, np.max(error), "o", label="{:d}".format(n))
+    axis[1].loglog(n, np.max(error), "o", label="exact, n={:d}".format(n))
 
     if exact:
         axis[0].plot(x, u, label="u(x)")
 
 
 if __name__ == "__main__":
-    fig, ax = plt.subplots(1, 2)
-    num_n = 3
-    exact = False
+    num_of_args = len(sys.argv)
+    fig, ax  = plt.subplots(1, 2)
+    
+    if num_of_args >= 2:
+        num_n    = num_of_args - 1
+        exact    = False
 
-    for i in range(1, num_n+1):
-        if i == num_n:
-            exact = True
-        plot(ax, 10**i, exact)
+        for i in range(1, num_n + 1):
+            if i == num_n:
+                exact = True
+
+            filename = str(sys.argv[i])
+            values   = np.loadtxt(filename, skiprows=1)
+            n        = len(values[:, 0]) - 2
+
+            plot_func(ax, n, args=values, exact=exact)
+
+    else:
+        num_n = 3
+        exact = False
+
+        for i in range(1, num_n + 1):
+            if i == num_n:
+                exact = True
+            plot_func(ax, 10**i, exact=exact)
     
     ax[0].legend()
     plt.show()
