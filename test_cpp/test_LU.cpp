27fe40825913221534d77cec1f464602921648e0
Mats Ola Sand <matsolasand@outlook.com>
2019-09-05 13:21:29 +0200

    Created an nxn matrix with armadillo and set the trigonal values.

--- test_cpp/test_LU.cpp
+++ test_cpp/test_LU.cpp
@@ -1,6 +1,29 @@
 #include <armadillo>
+#include <iostream>
 
 int main()
 {
+    int n_rows = 10;
+    int n_cols = 10;
+    arma::mat A(n_rows, n_cols);
+    for(int i=0; i<n_rows; i++)
+    {
+        for(int j=0; j<n_cols; j++){
+            if(i == j)
+            {
+                A(i, j) = 2.0;
+            }
+            else if(i == j + 1)
+            {
+                A(i, j) = 3.0;
+            }
+            else if(i == j - 1)
+            {
+                A(i, j) = 1.0;
+            }
+        }
+    }
+    A.print();
+    // std::cout << A.print() << std::endl ;
     return 1;
 }
