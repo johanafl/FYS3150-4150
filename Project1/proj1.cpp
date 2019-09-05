6a04fd444b202f92a62f4065171ab2852026aa30
Johan <johanafl@student.matnat.uio.no>
2019-09-05 18:02:44 +0200

    Added small comments to each of the functions

--- Project1/proj1.cpp
+++ Project1/proj1.cpp
@@ -12,17 +12,26 @@ double relative_error(double v, double u);
 void LU_arma(int n);
 
 double rhs_func(double x)
+/*
+Function for calculating the rhs, i.e., our f(x).
+*/
 {
     return 100*exp(-10*x);
 }
 
 double exact_solution(double x)
+/*
+Function for calculating the exact solution to our numerical problem.
+*/
 {
     return 1 - (1 - exp(-10))*x - exp(-10*x);
 }
 
 
 void gauss_elim(int n) // include diaginal variables
+/*
+Function for doing Gaussian elimination on our trigonal matrix.
+*/
 {
     double stepsize = 1.0/(n+1);
 
@@ -35,14 +44,8 @@ void gauss_elim(int n) // include diaginal variables
    for (int i=0; i<n; i++)
     {
         double x = stepsize*(i+1);
-        // std::cout << x << std::endl;
         rhs_val[i] = rhs_func(x)*(stepsize*stepsize);
     }
-    // std::cout << std::endl;
-    // for (int i=0; i<n; i++)
-    // {
-    //     std::cout << rhs_val[i] << std::endl;
-    // }
 
     for (int i=0; i<n-1; i++)
     {
@@ -53,44 +56,26 @@ void gauss_elim(int n) // include diaginal variables
     diag[n-1] = 2.0;
 
     ////////////////////////////////
-    // clock_t start, finish; // Gives only integer time. Not double!
-    // start = clock();
-
-    // time_t time0; // create timers.   // This also give wrong.
-    // time_t time1;
-    // time(&time0); // get current time.
-
     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now(); // Taken from this site: http://www.cplusplus.com/reference/chrono/steady_clock/
     ////////////////////////////////
 
     for (int i=0; i<n-1; i++)
     {
-        // std::cout << lower_diag[i] << std::endl;
-        // std::cout << upper_diag[i] << std::endl;
         diag[i+1] = diag[i+1] - lower_diag[i]/diag[i]*upper_diag[i];
         rhs_val[i+1] = rhs_val[i+1] - lower_diag[i]/diag[i]*rhs_val[i];
-        // std::cout << diag[i+1] << std::endl;
     }
 
     computed[n-1] = rhs_val[n-1]/diag[n-1];
     for (int i=n-1; i>=1; i--)
     {
-        // std::cout << lower_diag[i-1] << std::endl;
-        // std::cout << upper_diag[i-1] << std::endl;
-        // std::cout << diag[i-1] << std::endl;
-        // computed[i-1] = (rhs_val[i-1] - upper_diag[i-1]*computed[i])/diag[i-1];
         computed[i-1] = (rhs_val[i-1] - upper_diag[i-1]*computed[i])/diag[i-1];
     }
-    ////////////////////////////////
-    // finish = clock();
-    // double tot_time = (finish - start)/CLOCKS_PER_SEC;
 
-    // time(&time1);   // get current time after time pass.
-    // double tot_time = time1 - time0;
- 
+    ////////////////////////////////
     std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
     std::chrono::duration<double> tot_time = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
     ////////////////////////////////
+    
     std::cout << std::setprecision(32) << tot_time.count() << std::endl; // NB! Gives time in seconds.
 
     std::string filename = "Poisson_values_n_" + std::to_string(n) + ".txt";
@@ -103,70 +88,10 @@ void gauss_elim(int n) // include diaginal variables
     delete[] computed;
 }
 
+void gauss_elim_spes(int n)
 /*
-// Old solution:
-void gauss_elim(int n) 
-{
-    double h = 1.0/(n+1);
-
-    double* a      = new double[n-1]; 
-    double* b      = new double[n];   
-    double* c      = new double[n-1]; 
-    double* f      = new double[n];   
-    double* v      = new double[n];   
-    double* f_prim = new double[n];   
-    double* b_prim = new double[n];   
-
-   for (int i=0; i<n; i++)
-    {
-        double x = h*(i+1);
-        f[i] = func_f(x);
-    }
-
-
-    for (int i=0; i<n-1; i++)
-    {
-        a[i] = -1;
-        b[i] = 2;
-        c[i] = -1;
-    }
-    b[n-1] = 2;
-
-    b_prim[0] = b[0];
-    for (int i=0; i<n-1; i++)
-    {
-        b_prim[i+1] = b[i+1] - a[i]/b[i]*c[i];
-        f_prim[i+1] = f[i+1] - a[i]/b[i]*f[i];
-    }
-    // v[n-1] = f_prim[n-1];
-    // for (int i=n-1; i>=1; i--)
-    // {
-    //     v[i-1] = f_prim[i-1] - c[i-1]*v[i];
-    // }
-    for (int i=n-1; i>=1; i--)
-    {
-        f_prim[i-1] = f_prim[i-1] - c[i-1]/b_prim[i]*f_prim[i];
-    }
-    for (int i=0; i<n; i++)
-    {
-        v[i] = f_prim[i]/b_prim[i];
-    }
-    
-    std::string filename = "Poisson_values_n_" + std::to_string(n)
-                             + ".txt";
-    write_to_file(filename, n, v);
-
-    delete[] a;
-    delete[] b;
-    delete[] c;
-    delete[] f;
-    delete[] v;
-    delete[] f_prim;
-    delete[] b_prim;
-}
+Function for doing Gaussian elimination on our very special trigonal matrix.
 */
-
-void gauss_elim_spes(int n) 
 {
     double stepsize = 1.0/(n+1);
 
@@ -207,9 +132,9 @@ void gauss_elim_spes(int n)
     std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
     std::chrono::duration<double> tot_time = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
     ////////////////////////////////
+    
     std::cout << std::setprecision(32) << tot_time.count() << std::endl; // NB! Gives time in seconds.
 
-    // Maybe change the name of the file to indecate that the specialized function was used?
     std::string filename = "Poisson_values_spes_n_" + std::to_string(n) + ".txt";
     write_to_file(filename, n, computed);
 
@@ -218,65 +143,11 @@ void gauss_elim_spes(int n)
     delete[] computed;
 }
 
-/*
-void gauss_elim_spes(int n) 
-{
-    double h = 1.0/(n+1);
-
-    double* a      = new double[n-1]; 
-    double* b      = new double[n];   
-    double* c      = new double[n-1]; 
-    double* f      = new double[n];   
-    double* v      = new double[n];   
-    double* f_prim = new double[n];   
-    double* b_prim = new double[n];   
-
-   for (int i=0; i<n; i++)
-    {
-        double x = h*(i+1);
-        f[i] = func_f(x);
-    }
-
-
-    for (int i=0; i<n-1; i++)
-    {
-        a[i] = -1;
-        b[i] = 2;
-        c[i] = -1;
-    }
-    b[n-1] = 2;
-
-    b_prim[0] = b[0];
-    for (int i=0; i<n-1; i++)
-    {
-        b_prim[i+1] = 2 - 1/2; // (29.08.19) Are we allowed to assume b[i]=2 and precalculate b_prim[i]? (30.08.19) This is wrong!
-        f_prim[i+1] = f[i+1] - 1/2;
-    }
-    for (int i=n-1; i>=1; i--)
-    {
-        f_prim[i-1] = f_prim[i-1] - 1/b_prim[i]*f_prim[i];
-    }
-    for (int i=0; i<n; i++)
-    {
-        v[i] = f_prim[i]/b_prim[i];
-    }
-
-    int value = n;
-    std::string filename = "Poisson_values_n_" + std::to_string(n)
-                             + ".txt";
-    write_to_file(filename, n, v);
-
-    delete[] a;
-    delete[] b;
-    delete[] c;
-    delete[] f;
-    delete[] v;
-    delete[] f_prim;
-    delete[] b_prim;
-}
-*/
-
 void write_to_file(std::string filename, int n, double*computed_val)
+/*
+Function for writing the values to a .txt-file.
+Writing the exact and computed solution as well as the error for the computed.
+*/
 {
     double* exact_val = new double[n];
     double* eps       = new double[n];
@@ -319,6 +190,11 @@ void write_to_file(std::string filename, int n, double*computed_val)
 }
 
 void write_to_file(std::string filename, int n, arma::vec computed_val)
+/*
+Function for writing the values to a .txt-file.
+Writing the exact and computed solution as well as the error for the computed.
+This function is used when we use armadillo to calculate the computed solution.
+*/
 {
     double* computed = new double[n];
     for(int i=0; i<n; i++)
@@ -329,65 +205,19 @@ void write_to_file(std::string filename, int n, arma::vec computed_val)
     delete[] computed;
 }
 
-/*
-// Old solution
-void write_to_file(std::string filename, int n, double*v)
-{
-    double* u   = new double[n];
-    double* eps = new double[n];
-
-    double h    = 1.0/(n+1);
-
-    for (int i=0; i<n; i++)
-    {
-        double x = h*(i+1);
-        u[i]   = func_u(x);
-        if(u[i] == 0)
-        {
-            eps[i] = -1;
-        }
-        else
-        {
-            eps[i] = epsilon(v[i], u[i]);
-        }    
-    }
-
-    std::ofstream results_file;
-    results_file.open(filename);
-    results_file << std::setw(25) << "U(x) (exact)"
-                 << std::setw(25) << "V(x) (discretized)"
-                 << std::setw(25) << "Relative error \n"
-                 << std::setw(25) << 0
-                 << std::setw(25) << 0
-                 << std::setw(25) << 0 << std::endl;
-    for (int i=0; i<n; i++)
-    {
-        results_file << std::setw(25) << std::setprecision(16) << u[i]
-                     << std::setw(25) << std::setprecision(16) << v[i]
-                     << std::setw(25) << std::setprecision(16) << eps[i]
-                     << std::endl;
-    }
-    results_file << std::setw(25) << 0
-                 << std::setw(25) << 0
-                 << std::setw(25) << 0 << std::endl;
-    results_file.close();
-}
-*/
-
 double relative_error(double computed_val, double exact_val)
+/*
+Function for calculating the relative error for the computed solution relative
+to the exact solution.
+*/
 {
     return fabs((computed_val - exact_val)/exact_val);
 }
 
+void LU_arma(int n)
 /*
-// Old solution
-double epsilon(double v, double u)
-{
-    return (fabs((v - u)/u));
-}
+Function for calculating the computed values using the armadillo library.
 */
-
-void LU_arma(int n) 
 {
     double stepsize = 1.0/(n+1);
 
@@ -425,7 +255,6 @@ void LU_arma(int n)
     ////////////////////////////////
     std::cout << std::setprecision(32) << tot_time.count() << std::endl; // NB! Gives time in seconds.
 
-    // Maybe change the name of the file to indecate that the specialized function was used?
     std::string filename = "Poisson_values_LU_n_" + std::to_string(n) + ".txt";
     write_to_file(filename, n, computed);
 }
