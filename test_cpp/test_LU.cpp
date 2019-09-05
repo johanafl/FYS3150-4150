1da16ad88cf54df0b281a9e9f17c025706e97544
Mats Ola Sand <matsolasand@outlook.com>
2019-09-05 14:59:31 +0200

    Added LU decomposition to the test. Remember to run with -larmadillo flag!

--- test_cpp/test_LU.cpp
+++ test_cpp/test_LU.cpp
@@ -6,24 +6,15 @@ int main()
     int n_rows = 10;
     int n_cols = 10;
     arma::mat A(n_rows, n_cols);
-    for(int i=0; i<n_rows; i++)
-    {
-        for(int j=0; j<n_cols; j++){
-            if(i == j)
-            {
-                A(i, j) = 2.0;
-            }
-            else if(i == j + 1)
-            {
-                A(i, j) = 3.0;
-            }
-            else if(i == j - 1)
-            {
-                A(i, j) = 1.0;
-            }
-        }
-    }
-    A.print();
-    // std::cout << A.print() << std::endl ;
-    return 1;
+
+    A.diag(0)  += 2.0;
+    A.diag(1)  += -1.0;
+    A.diag(-1) += -1.0;
+
+    arma::mat L;
+    arma::mat U;
+    arma::lu(L, U, A);
+
+    U.print();
+    return 0;
 }
