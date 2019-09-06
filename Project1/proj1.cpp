df665641554927c2ada7cb8d468a88f8625bd1b8
Jon Dahl <jonkd@student.matnat.uio.no>
2019-09-06 21:01:18 +0200

    Added a compare time function. Modified thomas, thomas s, LU to return timing values. Strange problem with arma solver when solving for low n (sub 23) and many iterations.

--- Project1/proj1.cpp
+++ Project1/proj1.cpp
@@ -23,13 +23,18 @@ double exact_solution(double x)
 /*
 Function for calculating the exact solution to our numerical problem. This will
 be used for comparing the numerical precision for our computed solution.
+
+Parameters
+----------
+x : double
+    Function variable.
 */
 {
     return 1 - (1 - exp(-10))*x - exp(-10*x);
 }
 
 
-void thomas_algorithm(int n) // include diaginal variables
+void thomas_algorithm(int n, bool write)
 /*
 Function for doing Gaussian elimination on our trigonal matrix, i.e.,
 Thomas algorithm, and saving the the values to a .txt-file.
@@ -38,8 +43,14 @@ Since we have a trigonal matrix, this function will iterate over three vectors
 instead of the whole nxn matrix; one vector with n elements which will
 represent the diagonal elements, and two vectors with n-1 elements which will
 represent the upper and lower diagonal elements.
-This function will print out the time used by the gaussian elimination
-algorithm.
+
+Parameters
+----------
+n : int
+    Dimension of matrix. Number of discrete points.
+
+write : bool
+    Boolean for toggling write to file on/off.
 */
 {
     double stepsize = 1.0/(n+1);
@@ -80,12 +91,15 @@ algorithm.
     }
 
     std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-    std::chrono::duration<double> tot_time = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1);
+    std::chrono::duration<double> total_time = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1);
     
-    std::cout << std::setprecision(32) << tot_time.count() << std::endl; // NB! Gives time in seconds.
 
-    std::string filename = "Poisson_values_n_" + std::to_string(n) + ".txt";
-    write_to_file(filename, n, computed);
+    if (write) 
+    {
+        std::cout << std::setprecision(32) << total_time.count() << std::endl; // NB! Gives time in seconds.
+        std::string filename = "Poisson_values_n_" + std::to_string(n) + ".txt";
+        write_to_file(filename, n, computed);
+    }
 
     delete[] lower_diag;
     delete[] diag;
@@ -94,7 +108,7 @@ algorithm.
     delete[] computed;
 }
 
-void thomas_algorithm_special(int n)
+void thomas_algorithm_special(int n, bool write)
 /*
 Function for doing Gaussian elimination for the special trigonal matrix, i.e.,
 Thomas algorithm on a special trigonal matrix, and saving the the values to a
@@ -103,6 +117,11 @@ Thomas algorithm on a special trigonal matrix, and saving the the values to a
 This function works just like the general Thomas algorithm function, only here
 we don't need to include the upper and lower diagonal elements of the matrix
 since they er both just one, saving us some FLOPS.
+
+Parameters
+----------
+n : int
+    Dimension of matrix. Number of discrete points.
 */
 {
     double stepsize = 1.0/(n+1);
@@ -145,10 +164,14 @@ since they er both just one, saving us some FLOPS.
     std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
     std::chrono::duration<double> tot_time = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1);
 
-    std::cout << std::setprecision(32) << tot_time.count() << std::endl; // NB! Gives time in seconds.
 
-    std::string filename = "Poisson_values_spes_n_" + std::to_string(n) + ".txt";
-    write_to_file(filename, n, computed);
+
+    if (write) 
+    {   
+        std::cout << std::setprecision(32) << tot_time.count() << std::endl; // NB! Gives time in seconds.
+        std::string filename = "Poisson_values_spes_n_" + std::to_string(n) + ".txt";
+        write_to_file(filename, n, computed);
+    }
 
     delete[] diag;
     delete[] rhs_val;
@@ -160,22 +183,31 @@ void write_to_file(std::string filename, int n, double*computed_val)
 Function for writing the values to a .txt-file.
 Writing the exact and computed solution as well as the error for the computed
 relative to the exact solution.
+
+Parameters
+----------
+filename : std::string
+    A string input with the name of the file which data are written to.
+
+n : int
+    Dimension of matrix. Number of discrete points.
 */
 {
+    
     double* exact_val = new double[n];
     double* eps       = new double[n];
-
     double stepsize   = 1.0/(n+1);
+    double x;
 
     for (int i=0; i<n; i++)
-    
-    {
-        double x = stepsize*(i+1);
+    {   // calculating exact solution and relative error
+        x = stepsize*(i+1);
         exact_val[i] = exact_solution(x);
         eps[i] = relative_error(computed_val[i], exact_val[i]);
-           
+       
     }
 
+    // writing titles and first known values to file
     std::ofstream results_file;
     results_file.open(filename);
     results_file << std::setw(25) << "U(x) (exact)"
@@ -185,12 +217,14 @@ relative to the exact solution.
                  << std::setw(25) << 0
                  << std::setw(25) << 0 << std::endl;
     for (int i=0; i<n; i++)
-    {
+    {   // writing calculated values to file
         results_file << std::setw(25) << std::setprecision(16) << exact_val[i]
                      << std::setw(25) << std::setprecision(16) << computed_val[i]
                      << std::setw(25) << std::setprecision(16) << eps[i]
                      << std::endl;
     }
+
+    // writing final known values to file
     results_file << std::setw(25) << 0
                  << std::setw(25) << 0
                  << std::setw(25) << 0 << std::endl;
@@ -223,7 +257,7 @@ to the exact solution.
     return fabs((computed_val - exact_val)/exact_val);
 }
 
-void LU_arma(int n)
+double LU_arma(int n, bool write)
 /*
 Function for calculating the Thomas algorithm using the armadillo library.
 
@@ -245,14 +279,18 @@ This function also prints the time it takes to run the algorithm.
     arma::mat L;
     arma::mat U;
 
+    L.zeros();
+    U.zeros();
+
    for (int i=0; i<n; i++)
-    {
+    {   // calculating r.h.s. values
         double x = stepsize*(i+1);
         rhs_val(i) = rhs_func(x)*(stepsize*stepsize);
     }
 
     // starting timer
     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+
 
     arma::lu(L, U, A);
     arma::solve(tmp, L, rhs_val);
@@ -262,18 +300,64 @@ This function also prints the time it takes to run the algorithm.
     std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
     std::chrono::duration<double> tot_time = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1);
 
-    std::cout << std::setprecision(32) << tot_time.count() << std::endl; // NB! Gives time in seconds.
 
-    std::string filename = "Poisson_values_LU_n_" + std::to_string(n) + ".txt";
-    write_to_file(filename, n, computed);
+    if (write)
+    {
+        std::cout << std::setprecision(32) << tot_time.count() << std::endl; // NB! Gives time in seconds.
+        std::string filename = "Poisson_values_LU_n_" + std::to_string(n) + ".txt";
+        write_to_file(filename, n, computed);
+    }
+
+    return tot_time.count();
+
+
 }
+
+void compare_times()
+/*
+Strenge problem with arma solver error when n is low.
+*/
+{
+    int N[4];
+    
+    N[0] = 23;
+    N[1] = 100;
+    N[2] = 1000;
+    N[3] = 1000;
+
+    double LU_time;
+
+    std::string filename = "compare_times.txt";
+    std::ofstream compare_times_file;
+    compare_times_file.open(filename);
+    compare_times_file  << std::setw(38) << "thomas algorithm"
+                        << std::setw(38) << "thomas algorithm special"
+                        << std::setw(38) << "LU\n";
+
+    for (int i=0; i<1; i++)
+    {
+        for (int _=0; _<1000; _++)
+        {   
+            std::cout << _ << std::endl;
+            LU_arma(N[i], false);
+            // thomas_algorithm(N[i], false);
+            // thomas_algorithm_special(N[i], false);
+            // compare_times_file << std::setw(20) << std::setprecision(32) 
+            //                 << LU_time << std::endl;
+        }
+    }
+
+    compare_times_file.close();
+}
+
 
 int main(int argc, char *argv[])
 {
     int n = atoi(argv[1]);
-    thomas_algorithm(n);
+    // thomas_algorithm(n);
     // thomas_algorithm_special(n);
     // LU_arma(n);
+    compare_times();
  
     return 1;
 }
