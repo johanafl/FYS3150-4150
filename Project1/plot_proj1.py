ae508ba234b6a34ba40878382b64a0389a8e5242
Jon Dahl <jonkd@student.matnat.uio.no>
2019-09-08 17:29:35 +0200

    Made a new visualization function for computed and exact data.

--- Project1/plot_proj1.py
+++ Project1/plot_proj1.py
@@ -6,14 +6,16 @@ import os
 def compare_times():
     """
     Function for plotting and comparing timing values for the different
-    algorithms.
+    algorithms. Reads data from text file computed by accompanying C++ program.
+    Computes mean values for timings of same grid size and same algorithm, and
+    presents them as a plot.
     """
 
-    data = np.loadtxt("compare_times.txt", skiprows=1)
+    timing_data = np.loadtxt("compare_times.txt", skiprows=1)
 
-    runs = int(data[0][0])            # number of runs for each grid size value
-    num_grid_values = int(data[1][0]) # number of grid size values
-    data = data[2:]                   # slicing away 'runs' and 'num_grid_values'
+    runs = int(timing_data[0][0])            # number of runs for each grid size value
+    num_grid_values = int(timing_data[1][0]) # number of grid size values
+    timing_data = timing_data[2:]            # slicing away 'runs' and 'num_grid_values'
 
     thomas      = np.zeros(num_grid_values)
     thomas_s    = np.zeros(num_grid_values)
@@ -22,21 +24,109 @@ def compare_times():
     grid_values = np.zeros(num_grid_values)
 
     for i in range(num_grid_values):
-        tmp = data[i*(runs+1):(i+1)*(runs+1), :]
+        # loops over the timing data and calculates mean values
+        tmp = timing_data[i*(runs+1):(i+1)*(runs+1), :]
         grid_values[i] = int(tmp[0][0])
         thomas[i], thomas_s[i], LU[i] = np.mean(tmp[1:], axis=0)
 
     LU[np.where(LU == -1)] = np.nan     # all -1 values are values not computed
+                                        # set to nan so that plot is unaffected
 
-    plt.loglog(grid_values, thomas, label="Thomas")
+    plt.loglog(grid_values, thomas,   label="Thomas")
     plt.loglog(grid_values, thomas_s, label="Thomas special")
-    plt.loglog(grid_values, LU, label="LU")
-    plt.legend(loc="best")
+    plt.loglog(grid_values, LU,       label="LU")
+    
     plt.ylabel("Seconds")
     plt.xlabel("Grid points")
+    
+    plt.legend(loc="best")
     plt.grid()
     plt.tight_layout(pad=2)
+    
     plt.show()
+
+def visualize_error():
+    """
+
+    """
+    filenames = ["thomas_algorithm_error.txt",
+                 "thomas_algorithm_special_error.txt", "LU_error.txt"]
+    fig, ax = plt.subplots(figsize=(10,8))
+
+    for name in filenames:
+        n, error = np.loadtxt(name, unpack=True)
+
+    
+
+
+
+def visualize_data():
+    """
+    Function for reading data files, organizing them into arrays, and passing
+    the data to the plot function plot_data. The input text files contains
+    three columns. Column 1 is exact values, column 2 is computed values, and
+    column 3 is relative error.s
+    """
+
+    filenames   = ["thomas_algorithm_n_", "thomas_algorithm_special_n_", "LU_n_"]
+    grid_values = ["10", "100", "1000"]
+
+    for name in filenames:
+        # iterating over different algorithms
+        fig, ax = plt.subplots(figsize=(10,8))
+        
+        for value in grid_values:
+            # iterating over different number of grid points
+            filename = name + value + ".txt"    # fname for specific method and grid points
+            data     = np.loadtxt(filename, skiprows=1)
+            computed = data[:, 1]
+
+            if value == "1000":
+                # using only the best resolution for exact value
+                exact = data[:, 0]
+                plot_data(ax, computed, exact)
+            
+            else:
+                plot_data(ax, computed)
+
+        ax.legend(fontsize=18)
+        plt.tight_layout(pad=2.5)
+
+        ax.set_title(name[:-3], fontsize=18)
+        ax.set_xlabel("x", fontsize=18)
+        ax.set_ylabel("function value", fontsize=18)
+        ax.grid()
+
+        plt.show()
+
+        
+
+def plot_data(ax, computed, exact=False):
+    """
+    A simple function for automation of plotting of computed and exact values.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes._subplots.AxesSubplot object
+        Object on which to draw plots.
+    
+    computed : numpy.ndarray
+        Array containing computed values for plotting.
+
+    exact : bool, numpy.ndarray
+        Array containing exact values to plot. Defaults to False for no plot of
+        exact values.
+    """
+    
+    n = len(computed)
+    x = np.linspace(0, 1, n)
+    ax.plot(x, computed, label=n-2)
+    
+    if not isinstance(exact, bool):
+        ax.plot(x, exact, label="exact")
+
+
+
 
 
     
@@ -44,6 +134,31 @@ def compare_times():
 
 
 def plot_func(axis, n, args=False, exact=False, filename=False, method=True):
+    """
+    her skjer noe
+
+    parameters
+    ----------
+    axis : ax object from plt.subplots()
+
+    n : int
+        Number of grid points
+
+    kwargs
+    ------
+    args : False
+        kjører på vanlig måte, hvis ikke er det argumenter fra komandolinjen
+
+    exact : False
+        Hvis true plottes ekstakte løsningen for den gitte n verdien
+
+    filename : False
+        Filnavnet data skal hentes fra
+
+    method : True
+        hvis true plottes metoden 
+        hvis false plottes kun relativ error
+    """
 
     try:
         if not args:
@@ -61,14 +176,14 @@ def plot_func(axis, n, args=False, exact=False, filename=False, method=True):
         axis[0].plot(x, v, "-", label=method.format(n))
         axis[0].set_title("Numerical/exact solution", fontsize=18)
         axis[0].set_xlabel("x", fontsize=18)
-        axis[0].set_ylabel("u(x)/v(x)", fontsize=18)
+        axis[0].set_ylabel("function value", fontsize=18)
         axis[1].loglog(n, np.max(error), "o", label="exact, n={:d}".format(n))
         axis[1].set_title(r"Relative error", fontsize=18)
-        axis[1].set_xlabel("n", fontsize=18)
+        axis[1].set_xlabel("number of grid points, n", fontsize=18)
         axis[1].set_ylabel(r"$\epsilon_{max}(n)$", fontsize=18)
 
         if exact:
-            axis[0].plot(x, u, label="u(x)")
+            axis[0].plot(x, u, label="exact")
     
     else:
         epsilon_general = args
@@ -84,88 +199,6 @@ def plot_func(axis, n, args=False, exact=False, filename=False, method=True):
         # axis[1].set_ylabel("Relative error")
 
 if __name__ == "__main__":
-    num_of_args = len(sys.argv)
-
-    arg = np.array(["Methheds", "Epstasy"])
-    run = arg[0]
+    # visualize_data()
+    visualize_error()
     
-    # if num_of_args >= 2:
-    #     num_n    = num_of_args - 1
-    #     exact    = False
-
-    #     for i in range(1, num_n + 1):
-    #         if i == num_n:
-    #             exact = True
-
-    #         filename = str(sys.argv[i])
-    #         values   = np.loadtxt(filename, skiprows=1)
-    #         n        = len(values[:, 0]) - 2
-
-    #         plot_func(ax, n, args=values, exact=exact)
-        
-    #     ax[0].legend()
-    #     plt.show()
-
-    if run == arg[0]:
-        fig, ax   = plt.subplots(1, 2)
-        filenames = np.array(["Poisson_values_n_{:d}.txt",
-                              "Poisson_values_spes_n_{:d}.txt",
-                              "Poisson_values_LU_n_{:d}.txt"])
-        methods   = np.array(["General Thomas n={:d}", "Special Thomas n={:d}",
-                              "LU armadillo n={:d}"])
-
-        num_n = 3
-        exact = False
-
-        for j in range(3):
-            exact=False
-            for i in range(1, num_n + 1):
-                if i == num_n:
-                    exact = True
-                plot_func(ax, 10**i, exact=exact, filename=filenames[j],
-                            method=methods[j])
-    
-            ax[0].legend()
-            plt.show()
-    
-    else:
-        fig, ax  = plt.subplots()
-        
-        num_n      = 71
-        n_array    = np.linspace(1e1, 1e7, 61)
-
-        # error_gen  = np.zeros(num_n-10)
-        # error_spes = np.zeros(num_n-10)
-
-        # bar = IncrementalBar("Processing", max=61)
-
-        # errors = open("relative_errors.txt", "w")
-        # errors.write("Thomas              |   Thomas special\n")
-
-        # for i in range(10, num_n):
-        #     # str = "./proj1_test.out {:d}".format(int(10**(i/10.0)))
-        #     # os.system(str)
-        #     values_gen    = np.loadtxt("Poisson_values_n_{:d}.txt"
-        #                             .format(int(10**(i/10.0))), skiprows=1)
-        #     values_spes   = np.loadtxt("Poisson_values_spes_n_{:d}.txt"
-        #                             .format(int(10**(i/10.0))), skiprows=1)
-        #     error_gen[i-10]  = np.max(values_gen[:, 2])
-        #     error_spes[i-10] = np.max(values_spes[:, 2])
-        #     errors.write(str(error_gen[i-10]) + "   " + str(error_spes[i-10])
-        #                  + "\n")
-
-        #     bar.next()
-        # bar.finish()
-        # errors.close()
-
-        relative_errors = np.loadtxt("relative_errors.txt", skiprows=1)
-        error_general   = relative_errors[:, 0]
-        error_special   = relative_errors[:, 1]
-        
-        for i in range(2):
-            plot_func(ax, n_array, args=error_general, filename=error_special,
-                        method=False)
-    
-            ax.legend(loc="best")
-            plt.show()
-        #plt.savefig("$Maximum relative error per n")
