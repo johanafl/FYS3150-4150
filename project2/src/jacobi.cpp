5d448629fb247a1c925ac2c287a8552831de656d
Johan <johanafl@student.matnat.uio.no>
2019-09-13 21:18:18 +0200

    Gone away from class. Startet with initializing matrix and finding max element.

--- project2/src/jacobi.cpp
+++ project2/src/jacobi.cpp
@@ -7,88 +7,111 @@
 #include <chrono>
 
 
-// class Jacobi {
-// /*
-// Class for finding eigenvalues of matrix using Jacobi's method.
-// */
-// private:
-//     // arma::mat orto_transform_mat;
-//     arma::mat mat;
-//     int n;
-//     double eps;
 
-// public:
-//     Jacobi(int size, arma::mat original_matrix, double tolerance) {
-//     /*
-//     Constructer of the Jacobi class. 
-//     */
+void create_transformation_matrix(double theta, int l) {
+/*
+Not implemented yet. Supposed to create an orthogonal transformation matrix.
+Think this function is unnessesary.
+*/
+}
 
-//         arma::mat orto_transform_mat(size, size);
-//         orto_transform_mat.zeros();
-//         orto_transform_mat.diag(0) += 1.0;
-//     }
+void step_forward() {
+/*
+Not implemented yet. Perhaps call the create_transformation_matrix. Supposed to 
+multiply the transformation matrix with the matrix we have. 
+*/
+    // Compute next step
+}
 
-//     void create_transformation_matrix(double theta, int l) {
-//     /*
 
-//     */
-    
-//     }
+void find_eigenvalues() {
+/*
+Not implemented yet. Check if norm is less than epsilon. If not, continue to 
+find eigenvalues
+*/
+    // (old)    implement a while loop. norm(mat,"fro") should take the frobenius norm (http://arma.sourceforge.net/docs.html)
+    // (update) We are supposed to use the maximum value. use the find_max function.
 
-//     void step_forward() {
-//     /*
-//     Perhaps call the create_transformation_matrix
-//     */
-//         // Compute next step
-//     }
-
-    // void find_max() {
-    //     /*
-    //     Finds the max value of the off-diagonal elements of the matrix. Saves
-    //     the indexes of the max value.
-    //     */
-
-    //     double max_val = 0;     // final max value
-    //     double check_max;       // placeholder for new max value to check
-    //     int max_row;            // row position of max value
-    //     int max_column;         // column position of max value
-
-    //     for (int row=0; i<n; i++)
-    //     {   // loops over rows
-            
-    //         for (int column=0; j<n; j++)
-    //         {   // loops over columns
-    //             check_max = original_matrix(i,j);
-                
-    //             if (check_max > max_val)
-    //             {
-    //                 max_val = check_max;
-    //                 max_row = row;
-    //                 max_column = column;
-    //             }
-    //         }
-    //     }
+    // while ( > tolerance)
+    // {
+    //     step_forward();
     // }
+}
 
-//     void find_eigenvalues() {
-//     /*
-//     Check if norm is less than epsilon. If not, continue to find eigenvalues
-//     */
-//         // implement a while loop. norm(mat,"fro") should take the frobenius norm (http://arma.sourceforge.net/docs.html)
-//         while (norm(mat,"fro") > tolerance)
-//         {
-//             step_forward();
-//         }
-//     }
 
-//     ~Jacobi() {
-//     /*
-//     Deallocating all allocated objects.
-//     */
-//     // delete[] orto_transform_mat;
-//     }
-// };
 
+arma::mat construct_diag_matrix(int n) {
+    /*
+    Function for creating a diagonal armadillo matrix.
+    */
+    arma::mat A(n, n);
+    A.zeros();
+
+    A.diag(0) += 2;
+    A.diag(-1) += -1.0;
+    A.diag(1) += -1.0;
+
+    return A;
+}
+
+arma::mat construct_diag_matrix(int n, double up_low_diag, arma::vec diag) {
+    /*
+    Function for creating a diagonal armadillo matrix.
+    */
+    arma::mat A(n, n);
+    A.zeros();
+
+    A.diag(0) = diag;
+    A.diag(-1) += up_low_diag;
+
+    return A;
+}
+
+double find_max(int n, arma::mat A, int idx_row, int idx_column) {
+    /*
+    Finds the max value of the off-diagonal elements of the matrix. Saves
+    the indexes of the max value.
+
+    Parameters
+    ----------
+    A : arma::mat
+        Matrix of which we want to find the maximum element.
+
+    idx_row : int
+        Index of the row with maximum element.
+
+    idx_column : int
+        Index of the column with maximum element.
+
+
+    Returns
+    -------
+    max_val
+    */
+
+    double max_val = 0;     // final max value
+    double matrix_elemet;   // placeholder for matrix element value to check
+
+    for (int row=0; row<n; row++)
+    {   // loops over rows
+        
+        for (int column=0; column<n; column++)
+        {   // loops over columns
+            if (row != column)
+            {
+                matrix_elemet = fabs(A(row,column));
+                
+                if (matrix_elemet > max_val)
+                {
+                    max_val = matrix_elemet;
+                    idx_row = row;
+                    idx_column = column;
+                }
+            }
+        }
+    }
+    return max_val;
+}
 
 void test() {
     /*
@@ -97,21 +120,13 @@ void test() {
 
 }
 
-void construct_matrix() {
-
-    int step;
-    int n = 10;
-    arma::mat A(n, n);
-    A.zeros();
-
-    A.diag(0) += 1;
-    A.diag(-1) += -1.0/(step*step);
-
-}
 
 
 int main(int argc, char* argv[]) {
+    int n = 5;
+    double stepsize = 1/(n+1);
 
-    construct_matrix();
+    arma::mat A = construct_diag_matrix(n);
+    A.print();
     return 1;
 }
