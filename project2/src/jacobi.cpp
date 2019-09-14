7b7f667857783eba0cca59fa2b282ffe58165954
Johan <johanafl@student.matnat.uio.no>
2019-09-14 17:31:36 +0200

    Roughly implemented transform function and checked that the program (with the test function) runs.

--- project2/src/jacobi.cpp
+++ project2/src/jacobi.cpp
@@ -8,19 +8,19 @@
 
 
 void step_forward() {
-/*
-Not implemented yet. Perhaps call the create_transformation_matrix. Supposed to 
-multiply the transformation matrix with the matrix we have. 
-*/
+    /*
+    Not implemented yet. Perhaps call the create_transformation_matrix. Supposed to 
+    multiply the transformation matrix with the matrix we have. 
+    */
     // Compute next step
 }
 
 
 void find_eigenvalues() {
-/*
-Not implemented yet. Check if norm is less than epsilon. If not, continue to 
-find eigenvalues
-*/
+    /*
+    Not implemented yet. Check if norm is less than epsilon. If not, continue to 
+    find eigenvalues
+    */
     // (old)    implement a while loop. norm(mat,"fro") should take the frobenius norm (http://arma.sourceforge.net/docs.html)
     // (update) We are supposed to use the maximum value. use the find_max function.
 
@@ -109,28 +109,50 @@ double find_max(int n, arma::mat A, int* idx_row, int* idx_column) {
 }
 
 
-void transform(arma::mat* A, int idx_col, int idx_row) {
+void transform(int n, arma::mat* A, int idx_col, int idx_row) {
+    /*
+    This function rotatetes the (symmetric) matrix A by an angle theta in the 
+    plane spanned by the unit vectors e_l and e_k, where 
+    e_i = [0,0,...,0,1,0,...,0]. The angle theta is defined s.t. that the 
+    elements a_lk = a_kl = 0 after the transformation.
+
+    Parameters
+    ----------
+    A : arma::mat*
+        Pointer to the symmetric matrix A
+    idx_row : int
+        The index k of one of the unit vectors that are used in the rotation.
+    idx_col : int
+        The index l of the other unit vector that are used in the rotation.
+    */
+    arma::mat B = *A;
+    double a_ll = B(idx_row, idx_row);
+    double a_kk = B(idx_col, idx_col);
+    double a_kl = B(idx_col, idx_row);
+
+    double tau = (a_ll - a_kk)/(2*a_kl); // We have defined tau = cot(2*theta), where theta is unknown. We are choosing thata s.t. the element b_kl = b_lk = 0, where B is the new matrix after the transformation with elements b_ij.
+    double t = -tau + std::sqrt(1 + tau*tau); // We have defined t = tan(theta). We could have choosen minus instead of pluss in front of the square root, and gotten the same result (by virtue of the algorithm).
+    double c = 1/std::sqrt(1 + t*t);
+    double s = t*c;
+
+    for (int i=0; i<n; i++) // NB! In project description: idx_row = k and idx_col = l.
+    {
+        B(i,idx_row) = B(idx_row,i) = B(idx_row,i)*c - B(idx_col,i)*s;
+        B(i,idx_col) = B(idx_col,i) = B(idx_col,i)*c + B(idx_row,i)*s;
+    }    
+    // B(idx_col,idx_row) = B(idx_row,idx_col) = 0; // By virtue of the algorithm (this is how we found theta!).
+    B(idx_row,idx_row) = B(idx_row,idx_row)*c*c - 2*B(idx_col,idx_row)*c*s + B(idx_col,idx_col)*s*s;
+    B(idx_col,idx_col) = B(idx_col,idx_col)*c*c - 2*B(idx_col,idx_row)*c*s + B(idx_row,idx_row)*s*s;
+}
+
+void find_eig(arma::mat A){
     /*
     Not implemented yet.
     */
-    double a_ll = A(idx_row, idx_row);
-    double a_kk = A(idx_col, idx_col);
-    double a_kl = A(idx_col, idx_row);
-
-    double tau = (a_ll - a_kk)/(2*a_kl);
-
-    t = -tau + std::sqrt(1 + tau*tau);
-    
+    // int idx_col;
+    // int idx_row;
+    // double max_val = find_max(A);
 }
-
-// void find_eig(arma::mat A){
-//     /*
-//     Not implemented yet.
-//     */
-//     int idx_col;
-//     int idx_row;
-//     double max_val = find_max(A);
-// }
 
 void test_find_max() {
     /*
@@ -171,7 +193,8 @@ void test_inner_product_conserved() {
     Checks that the inner product is preserved after one rotation with the
     transformation matrix.
     */
-    arma::mat A(3,3);
+    int n = 3;
+    arma::mat A(n,n);
     A.zeros();
     A(0, 2) = 1;
     A(1, 0) = 1;
@@ -181,13 +204,10 @@ void test_inner_product_conserved() {
     int idx_row = 0;
     int idx_column = 2;
 
-    // find_max(A, idx_row, idx_row);
-
-
-    // transform(A, idx_row, idx_column); // This must be implemented.
+    transform(n, & A, idx_row, idx_column); // This must be implemented.
     double inner_prod = arma::dot(A.col(0),A.col(1));
     
-    if (fabs(inner_prod) < tol)
+    if (fabs(inner_prod) > tol)
     {
         std::cout << "Something went wrong!" << std::endl;
     }
@@ -195,13 +215,14 @@ void test_inner_product_conserved() {
 
 
 int main(int argc, char* argv[]) {
-    int n = 5;
-    double stepsize = 1/(n+1);
+    // int n = 5;
+    // double stepsize = 1/(n+1);
 
 
     // arma::mat A = construct_diag_matrix(n);
     // A.print();
     // test_find_max();
+    test_inner_product_conserved();
 
     return 1;
 }
