d1cc8cf5782bf378aecf5805350864f98cf0d6b5
Jon Dahl <jonkd@student.matnat.uio.no>
2019-09-19 14:37:01 +0200

    test_inner_product_conserved now works as it shoud with R^(3x3) basis vectors in an anti-diagonal matrix.

--- project2/src/jacobi.cpp
+++ project2/src/jacobi.cpp
@@ -15,9 +15,9 @@ arma::mat construct_diag_matrix(int n) {
     arma::mat A(n, n);
     A.zeros();
 
-    A.diag(0) += 2;
+    A.diag(0)  += 2;
     A.diag(-1) += -1.0;
-    A.diag(1) += -1.0;
+    A.diag(1)  += -1.0;
 
     return A;
 }
@@ -85,51 +85,64 @@ double find_max(int n, arma::mat A, int* idx_row, int* idx_column)
     return max_val;
 }
 
-void transform(int n, arma::mat* A, int idx_col, int idx_row) {
+void transform(int n, arma::mat& A, int idx_row, int idx_col) {
     /*
     This function rotatetes the (symmetric) matrix A by an angle theta in the 
     plane spanned by the unit vectors e_l and e_k, where 
-    e_i = [0,0,...,0,1,0,...,0]. The angle theta is defined s.t. that the 
+    e_i = [0, 0, ..., 0, 1, 0, ..., 0]. The angle theta is defined s.t. the 
     elements a_lk = a_kl = 0 after the transformation.
 
     Parameters
     ----------
-    A : arma::mat*
-        Pointer to the symmetric matrix A
+    A : arma::mat&
+        Reference to matrix A
+
     idx_row : int
         The index k of one of the unit vectors that are used in the rotation.
+
     idx_col : int
         The index l of the other unit vector that are used in the rotation.
 
-    MATRIX A WILL NEVER BE CHANGED, LOOPS FOREVER
     */
 
-    arma::mat B = *A;       // quick fix since pointing and indexing doesnt work as expected
-    
-    double a_kk = B(idx_row, idx_row);
-    double a_ll = B(idx_col, idx_col);
-    double a_lk = B(idx_col, idx_row);
+
+    double a_kk = A(idx_row, idx_row);
+    double a_ll = A(idx_col, idx_col);
+    double a_lk = A(idx_col, idx_row);
 
     double tau = (a_ll - a_kk)/(2*a_lk); // We have defined tau = cot(2*theta), where theta is unknown. We are choosing thata s.t. the element b_kl = b_lk = 0, where B is the new matrix after the transformation with elements b_ij.
-    double t   = -tau + std::sqrt(1 + tau*tau); // We have defined t = tan(theta). We could have choosen minus instead of pluss in front of the square root, and gotten the same result (by virtue of the algorithm).
+    double t;
+    
+    // to counteract numerical precision errors when tau is large
+    if (tau >= 0)
+    {
+        t = 1.0/(tau + std::sqrt(1 + tau*tau));
+    }
+    else
+    {
+        t = -1.0/(-tau + std::sqrt(1 + tau*tau));
+    }
+    
     double c   = 1/std::sqrt(1 + t*t);
     double s   = t*c;
-    double tmp;
+    double a_ki;
 
-    for (int i=0; i<n; i++) // NB! In project description: idx_row = k and idx_col = l.
+    for (int i=0; i<n; i++)
     {   // elements kk, ll, lk, kl should not be changed, but it is fixed after
         // the loop instead of using an if-test in the loop (vectorization)
         
-        tmp = B(idx_row, i);    // since this matrix element will be overwritten
+        a_ki = A(idx_row, i);    // since this matrix element will be overwritten
         
-        B(i, idx_row) = B(idx_row, i) = tmp*c - B(idx_col, i)*s;
-        B(i, idx_col) = B(idx_col, i) = B(idx_col, i)*c + tmp*s;
+        A(i, idx_row) = A(idx_row, i) = a_ki*c - A(idx_col, i)*s;
+        A(i, idx_col) = A(idx_col, i) = A(idx_col, i)*c + a_ki*s;
     }
 
-    B(idx_col, idx_row) = B(idx_row, idx_col) = 0; // By virtue of the algorithm (this is how we found theta!).
-    B(idx_row, idx_row) = a_kk*c*c - 2*a_lk*c*s + a_ll*s*s;
-    B(idx_col, idx_col) = a_ll*c*c - 2*a_lk*c*s + a_kk*s*s;
+    A(idx_col, idx_row) = A(idx_row, idx_col) = 0; // by virtue of the algorithm (this is how we found theta!).
+    A(idx_row, idx_row) = a_kk*c*c - 2*a_lk*c*s + a_ll*s*s;
+    A(idx_col, idx_col) = a_ll*c*c + 2*a_lk*c*s + a_kk*s*s; // check with Johan if + is correct
+
 }
+
 
 arma::mat find_eig(int n, arma::mat A, double tol)
 {
@@ -145,7 +158,7 @@ arma::mat find_eig(int n, arma::mat A, double tol)
     
     while (max_val > tol)
     {
-        transform(n, & B, idx_col, idx_row);    // no B is returned, runs forever
+        transform(n, B, idx_col, idx_row);    // no B is returned, runs forever
         max_val = find_max(n, B, & idx_col, & idx_row);
     }
     return B;
@@ -154,23 +167,26 @@ arma::mat find_eig(int n, arma::mat A, double tol)
 void test_find_max() {
     /*
     Test function that tests the most important functionality of the Jacobi class.
+
+    This test works.
     */
     
     
     int n = 5;                  // dimension of test matrix
     double maximum = 5555;      // arbitrary max value in matrix
     
-    arma::mat A(n,n);
+    arma::mat A(n, n);
     A.zeros();
     
-    for (int i=0; i<5; i++) {
-        
-        for (int j=0; j<5; j++) {
-            A(i, j) = i*j*1.5;
+    for (int i=0; i<5; i++)
+    {   // loops over rows
+        for (int j=0; j<5; j++)
+        {   // loops over columns
+            A(i, j) = i*j*1.5;  // populating the array with arbitrary values
         }
     }
 
-    A(3,2) = maximum;
+    A(3, 2) = maximum;  // placing the max value
     
     int idx_row;
     int idx_column;
@@ -188,6 +204,9 @@ void test_find_max() {
         std::cout << "Wrong indecies! Indecies was " << 3 << " and " << 2 
                   << ". Got " << idx_row << " and " << idx_column << std::endl;
     }
+
+
+    std::cout << max_val << std::endl;  // debug, remove later
 }
 
 
@@ -203,20 +222,29 @@ void test_inner_product_conserved() {
     A.zeros();
     
     A(0, 2) = 1;
-    A(1, 0) = 1;
-    A(2, 1) = 1;
+    A(1, 1) = 1;
+    A(2, 0) = 1;
     
-    double tol = 1;
+    double tol  = 1;
     int idx_row = 0;
-    int idx_column = 2;
+    int idx_col = 2;
 
-    transform(n, & A, idx_row, idx_column); // transform doesnt work
-    double inner_prod = arma::dot(A.col(0),A.col(1));
-    
-    if (fabs(inner_prod) > tol)
+    transform(n, A, idx_row, idx_col);
+
+    for (int i=0; i<n; i++)
     {
-        std::cout << "Something went wrong!" << std::endl;
+        for (int j=0; j<n; j++)
+        {
+            std::cout << arma::dot(A.col(i), A.col(j)) << std::endl;
+        }
     }
+
+    // std::cout << inner_prod << std::endl;
+    
+    // if (fabs(inner_prod) > tol)
+    // {
+    //     std::cout << "Something went wrong!" << std::endl;
+    // }
 }
 
 void test_find_eig() {
@@ -254,17 +282,6 @@ void test_find_eig() {
 }
 
 
-void test_test()
-{   
-    /*
-    This does not work.
-    */
-    arma::mat A(2, 2);
-    arma::mat* B = & A;
-
-    // std::cout << * B(0, 0) << std::endl;
-}
-
 int main(int argc, char* argv[]) {
     // int n = 5;
     // double stepsize = 1/(n+1);
@@ -273,10 +290,9 @@ int main(int argc, char* argv[]) {
     // arma::mat A = construct_diag_matrix(n);
     // A.print();
     // test_find_max();
-    // test_inner_product_conserved();
+    test_inner_product_conserved();
     // test_find_eig();
 
-    test_test();
 
     return 1;
 }
