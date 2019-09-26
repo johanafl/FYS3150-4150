f7712e93a679043b1d5396cd4df63ae6b6b195e1
Jon Dahl <jonkd@student.matnat.uio.no>
2019-09-26 20:54:37 +0200

    Loops over rho_max and grid values.

--- project2/src/quantum_dots.cpp
+++ project2/src/quantum_dots.cpp
@@ -3,53 +3,107 @@
 #include <iomanip>
 
 
-int main()
-{   
+void quantum_data()
+{   /*
 
-    int n = 100;                // grid size
+    Loops over rho_max and n (grid) values, calculates eigenvalues by the Jacobi
+    method, and writes data to a text file for visualization with Python.
+    */
+
+
+    bool progress = true;   // boolean for toggling progress info on/off
+    double eig;             // analytical eigenvalue
+    int n = 180;            // grid size
     double rho_min = 0;
-    double rho_max = 8.05;      // approx infty
-    double step = (rho_max - rho_min)/n;
-    double off_diag = -1/(step*step);
-    double tol_off_diag = std::pow(10, -5);
-
-    // constructing vector for the diagonal elements
-    arma::vec diag(n);
-    diag.zeros();
-
-    for (int i = 0; i < n; i++)
-    {   // creating the diagonal elements
-        diag(i) = 2/(step*step) + (rho_min + i*step)*(rho_min + i*step);
-    }
-
-    // constructing tri-diagonal matrix and vector for analytical eigenvalues
-    arma::mat A = construct_diag_matrix(n, off_diag, diag);
-    arma::vec analytical_eig(n);
-    analytical_eig(0) = 3;  // initial value
+    double rho_max = 5;         // approx infty
+    double tmp = rho_max;
+    double tol_off_diag = std::pow(10, -5); // tolerance for Jacobi
+    double step;
+    double off_diag;
     
-    // using Jacobis method to extract the eigenvalues of the tri-diagonal matrix
-    find_eig(n, A, tol_off_diag);
-    arma::vec sorted_diag = arma::sort(A.diag(0));
 
+    // loop-specific values
+    double d_rho   = 0.1;         // rho step size for incrementing in the loop
+    int num_eig    = 8;           // number of eigenvalues to write to file
+    double rho_end = 10;        // end rho_max value for the loop
+    double num_rho = (rho_end - rho_max)/d_rho; // number of rho_max values tested
+    int n_end = 200;    // end grid value
+    int dn    = 10;     // grid step size
 
+    // generating data file
     std::ofstream data_file;
-    data_file.open("eigenvalues.txt");
-    data_file << "calculated     exact     error\n";
+    data_file.open("eigenvalues.txt", std::ios_base::app);
+    data_file << num_eig << " " << num_rho << "\n";
+    data_file << "calculated     exact     error     rho_max     n\n";
 
-    
-    
-    for (int i = 0; i < 8; i++)
-    {   // writing data to file
-        
-        analytical_eig(i + 1) = analytical_eig(i) + 4;
-        data_file << std::setw(10) << sorted_diag(i);
-        data_file << std::setw(10) << analytical_eig(i);
-        data_file << std::setw(10) << fabs(analytical_eig(i) - sorted_diag(i));
-        data_file << "\n";
-
+    if (progress)
+    {   // for printing progress data
+        std::cout << "calculating" << std::endl;
+        std::cout << "===========" << std::endl;
     }
 
+    while (n < n_end)
+    {
+        while (rho_max < rho_end)
+        {   // looping over rho_max values and writing data to file
+            
+            eig = 3;    // initial analytical eigenvalue
+            step = (rho_max - rho_min)/n;
+            off_diag = -1/(step*step);
+
+            // constructing vector for the diagonal elements
+            arma::vec diag(n);
+            diag.zeros();
+
+            for (int i = 0; i < n; i++)
+            {   // creating the diagonal elements
+                diag(i) = 2/(step*step) + (rho_min + i*step)*(rho_min + i*step);
+            }
+
+            // constructing tri-diagonal matrix
+            arma::mat A = construct_diag_matrix(n, off_diag, diag);
+            
+            // using Jacobis method to extract the eigenvalues of the tri-diagonal matrix
+            find_eig(n, A, tol_off_diag);
+            arma::vec sorted_diag = arma::sort(A.diag(0));
+
+            if (progress)
+            {   // for printing progress data
+                std::cout << "rho_max: " << rho_max << " rho_end: ";
+                std::cout << rho_end << "  ";
+                std::cout << "n: " << n << " n_end: ";
+                std::cout << n_end << std::endl;
+            }
+
+
+            for (int i = 0; i < num_eig; i++)
+            {   // writing data to file
+                
+                data_file << std::setw(15) << sorted_diag(i);
+                data_file << std::setw(15) << eig;
+                data_file << std::setw(15) << fabs(eig - sorted_diag(i));
+                data_file << std::setw(15) << rho_max;
+                data_file << std::setw(15) << n;
+                data_file << "\n";
+                eig = eig + 4;
+
+            }
+
+
+            rho_max = rho_max + d_rho;
+
+        }
+        
+        rho_max = 5;
+        n += dn;
+    }
+    
     data_file.close();
 
+}
+
+int main()
+{   
+    quantum_data();
     return 0;
 }
