9c5bc8506fdb04ce50725a84bf7e8132defc7f8f
Jon Dahl <jonkd@student.matnat.uio.no>
2019-09-26 14:05:20 +0200

    Calculated eigenvalues are close, but we need fine-tuning of rho_max and grid. Comparison with analytical values is incomplete at the time.

--- project2/src/quantum_dots.cpp
+++ project2/src/quantum_dots.cpp
@@ -4,28 +4,36 @@
 int main()
 {   
 
-    int n = 10;
+    int n = 100;                // grid size
     double rho_min = 0;
-    double rho_max = 5;    // approx infty
+    double rho_max = 8.05;      // approx infty
     double step = (rho_max - rho_min)/n;
     double off_diag = -1/(step*step);
+    double tol_off_diag = std::pow(10, -5);
 
+    // constructing vector for the diagonal elements
     arma::vec diag(n);
     diag.zeros();
-    diag.print();
-
 
     for (int i = 0; i < n; i++)
     {   // creating the diagonal elements
         diag(i) = 2/(step*step) + (rho_min + i*step)*(rho_min + i*step);
     }
 
-    double diag2 = 10;
+    // constructing tri-diagonal matrix and vector for analytical eigenvalues
+    arma::mat A = construct_diag_matrix(n, off_diag, diag);
+    arma::vec analytical_eig(n);
+    
+    // using Jacobis method to extract the eigenvalues of the tri-diagonal matrix
+    find_eig(n, A, tol_off_diag);
+    arma::vec sorted_diag = arma::sort(A.diag(0));
 
-    // arma::mat A = construct_diag_matrix(n, off_diag, diag2);
+    for (int i = 0; i < 5; i++)
+    {   
+        analytical_eig(i) = diag(i) + 2*off_diag*std::cos(i*3.1415/(n + 1));
+        
+        std::cout << sorted_diag(i) << "  " << analytical_eig(i) << std::endl;
+    }
 
-    test_function_for_checking_header_implementation();
-
-    std::cout << "lol" << std::endl;
     return 0;
 }
