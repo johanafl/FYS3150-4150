62eab8db4eaf051002b922acbb2152216b586c9e
Jon Dahl <jonkd@student.matnat.uio.no>
2019-09-28 21:01:57 +0200

    We have outsourced a lot of code to a new file. Reverted this back to a similar state for when it was just for task 2d.

--- project2/src/quantum_dots.cpp
+++ project2/src/quantum_dots.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 
-arma::vec generate_diagonals(int grid, double step, double rho_min, double freq, int coulomb)
+arma::vec effective_potential(int grid, double step, double rho_min)
 {
     /*
     Creates a vector containing correct diagonal elements for task 2d.
@@ -18,26 +18,13 @@ arma::vec generate_diagonals(int grid, double step, double rho_min, double freq,
 
     rho_min : double
         Minimum rho value.
-
-    freq : double
-        A freqency in front of the harmonic oscillator potential. 
-        For the case of two electrons this normalizes the constants in the 
-        coulomb potential and makes it unitless.
-    
-    coulomb : int, either 1 or 0
-        A variable to indicate if we want to use an effictive potential with the
-        coulomb potential. 
-        Set to 1 to include.
-        Set to 0 to exclude.
     */
 
     arma::vec diag(grid);
 
     for (int i = 0; i < grid; i++)
     {   // creating the diagonal elements
-        diag(i) =   2/(step*step) 
-                  + freq*freq*(rho_min + i*step)*(rho_min + i*step) // Harmonic oscillator potential (with frequency)
-                  + coulomb/(rho_min + i*step);                     // Coulomb potential
+        diag(i) = 2/(step*step) + (rho_min + i*step)*(rho_min + i*step);
     }
 
     return diag;
@@ -82,58 +69,26 @@ private:
     int grid       = 100;      // grid size
     double rho_min = std::pow(10, -7);
     double rho_max = 5;        // approximating infinity
-    double tol_off_diag = std::pow(10, -5); // tolerance for Jacobi
+    double tol_off_diag = std::pow(10, -10); // tolerance for Jacobi
 
     // loop-specific values
     double rho_tmp = rho_max; // for reverting rho_max to original max value
-    double rho_end = 5.6;     // end rho_max value for the loop
+    double rho_end = 5.1;     // end rho_max value for the loop
     int grid_tmp   = grid;    // for reverting n to original max value
-    int grid_end   = 130;     // end grid value
+    int grid_end   = 120;     // end grid value
     double d_rho   = 0.1;     // rho step size for incrementing in the loop
     int d_grid     = 10;      // grid step size
     int num_eig    = 8;       // number of eigenvalues to write to file per rho
     double num_rho = (rho_end - rho_max)/d_rho; // number of rho_max values tested
     int num_grid   = (grid_end - grid)/d_grid;  // number of grid points tested
 
+    std::ofstream data_file;
+
 public:
     QuantumData()
     {
-        /* Empty right now. Might include accesibility for useres later.*/
-    }
-
-    void set_grid_values(int n, int end, double stepsize)
-    {
-        /* REMEMBER TO COMMENT*/
-        grid = n;
-        grid_end = end;
-        d_grid = stepsize;
-        grid_tmp = grid;
-        num_grid = (grid_end - grid)/d_grid;
-    }
-
-    void set_rho_values(double my_man_rho, double the_rho_has_landed, double see_you_later_rho_i_gator, double sthep_that_steip)
-    {
-        /* REMEMBER TO COMMENT*/
-        rho_min = my_man_rho;
-        rho_max = the_rho_has_landed;
-        rho_tmp = rho_max;
-        rho_end = see_you_later_rho_i_gator;
-        d_rho   = sthep_that_steip;
-        num_rho = (rho_end - rho_max)/d_rho;
-    }
-
-    void set_progress_values(bool do_you_really_want_to_see_this, bool of_course_i_do, bool or_mayby_not)
-    {
-        /* REMEMBER TO COMMENT*/
-        progress     = do_you_really_want_to_see_this;
-        looping_grid = of_course_i_do;
-        looping_freq = or_mayby_not;
-    }
-
-    void write_title_to_file(std::ofstream &data_file)
-    {
         // opening data file and writing title
-        // data_file.open("eigenvalues2.txt", std::ios_base::app);
+        data_file.open("eigenvalues2.txt", std::ios_base::app);
         data_file << num_eig << " " << num_rho << " " << num_grid << "\n";
         data_file << std::setw(20) << "calculated";
         data_file << std::setw(20) << "exact";
@@ -142,7 +97,59 @@ public:
         data_file << std::setw(20) << "n\n";
     }
 
-    void loop_rho(std::ofstream &data_file, double freq, int coulomb)
+    void set_grid_values(int grid_input, int grid_end_input, double d_grid_input)
+    {   /* 
+        For setting grid values. If not called, values are set to default.
+
+        Parameters
+        ----------
+        grid_input : int
+            Grid point start value.
+
+        grid_end_input : int
+            Grid point end value.
+
+        d_grid_input : double
+            Grid point step size
+        */
+        
+        grid = grid_input;
+        grid_end = grid_end_input;
+        d_grid = d_grid_input;
+        grid_tmp = grid;
+        num_grid = (grid_end - grid)/d_grid;
+    }
+
+    void set_rho_values(double rho_max_input, double rho_end_input, double d_rho_input)
+    {   /*
+
+        For setting rho values. If not called, values are set to default.
+        
+        Parameters
+        ----------
+        rho_max_input : double
+            rho max start value.
+
+        rho_end_input : double
+            rho max end value.
+
+        d_rho_input : double
+            rho max step size.
+        */
+        rho_max = rho_max_input;
+        rho_tmp = rho_max;
+        rho_end = rho_end_input;
+        d_rho   = d_rho_input;
+        num_rho = (rho_end - rho_max)/d_rho;
+    }
+
+    void set_progress_values(bool progress_input)
+    {
+        /* REMEMBER TO COMMENT*/
+        progress = progress_input;
+    }
+
+    void loop_rho()
     {   /*
         Loops over rho_max values.
         */
@@ -159,7 +166,7 @@ public:
             off_diag = -1/(step*step);
 
             // constructing vector for the diagonal elements            
-            arma::vec diag = generate_diagonals(grid, step, rho_min, freq, coulomb);
+            arma::vec diag = effective_potential(grid, step, rho_min);
 
             // constructing tri-diagonal matrix
             arma::mat A = construct_diag_matrix(grid, off_diag, diag);
@@ -190,7 +197,7 @@ public:
         }
     }
 
-    void loop_grid(std::ofstream &data_file, double freq, int coulomb)
+    void loop_grid()
     {   /*
         Loops over grid (n) values.
         */
@@ -202,50 +209,16 @@ public:
 
         while (grid < grid_end)
         {
-            loop_rho(data_file, freq, coulomb);
+            loop_rho();
             
             rho_max = rho_tmp;
             grid   += d_grid;
         }
     }
 
-    void loop_grid()
-    {   /*
-        Loops over grid (n) values.
-        */
-
-        std::ofstream data_file;
-        data_file.open("eigenvalues2.txt", std::ios_base::app);
-        loop_grid(data_file, 1, 0);
+    ~QuantumData()
+    {
         data_file.close();
-    }
-
-    void loop_frequency()
-    {   /*
-        Loops over frequencies (w).
-        */
-    
-        std::string filename;
-        std::ofstream data_file;
-        double freq[4] = {0.01, 0.5, 1, 5};
-
-        if (progress)
-        {   // progress information
-            std::cout << "looping over frequency, grid and rho max" << std::endl;
-        }
- 
-        for (int i = 0; i < 4; i++)
-        {   
-            filename = "eigenvalues_w_" + std::to_string(freq[i]) + ".txt";
-            data_file.open(filename, std::ios_base::app);
-            write_title_to_file(data_file);
-            
-            loop_grid(data_file, freq[i], 1);
-            
-            grid = grid_tmp;
-            data_file.close();
-        }
-
     }
 
 };
@@ -253,7 +226,9 @@ public:
 int main()
 {   
     QuantumData q;
-    q.loop_frequency();
+    q.set_grid_values(100, 101, 10);
+    q.set_rho_values(1, 7, 0.1);
+    q.loop_grid();
 
     return 0;
 }
