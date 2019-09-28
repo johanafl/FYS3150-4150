ed8efea5bb59574ccc501fc8f637a0b8d3be8c61
Jon Dahl <jonkd@student.matnat.uio.no>
2019-09-28 20:12:15 +0200

    Better comments, removed old unused method, fixed the comparison_plot method to compare max error for three different grid point values.

--- project2/src/quantum_dots.py
+++ project2/src/quantum_dots.py
@@ -16,133 +16,143 @@ class VisualizeData:
         """
 
         try:
-            num_eig, num_rho, num_n = np.loadtxt(filename, max_rows=1)
-            self.calc, self.exact, self.error, self.rho_max, self.n = \
+            num_eig, num_rho, num_grid = np.loadtxt(filename, max_rows=1)
+            self.calc, self.exact, self.error, self.rho_max, self.grid = \
                 np.loadtxt(filename, skiprows=2, unpack=True)
 
         except:
-            raise TypeError("Could not read file with filename {}.\nPerhaps this is not a file with eigenvalues/errors for a quantum mechanical system?".format(filename))
+            msg = (f"Could not read file with filename {filename}.\n"
+                "Perhaps this is not a file with eigenvalues/errors for a "
+                "quantum mechanical system?")
+            raise TypeError(msg)
 
-        self.num_eig = int(num_eig)      # number of eigenvalues
-        self.num_rho = int(num_rho)      # number of rho_max values
-        self.num_n   = int(num_n)        # number of grid point values
+        self.num_eig  = int(num_eig)    # number of eigenvalues
+        self.num_rho  = int(num_rho)    # number of rho_max values
+        self.num_grid = int(num_grid)   # number of grid point values
 
-    def contour_plot(self):
+    def contour_plot(self, selection="max"):
         """
         Generates contour plot of grid points vs rho_max, and the relative error
         as height values.
+
+        Parameters
+        ----------
+
+        selection : str, int
+            For choosing which selection of eigenvalues to visualize. 'min' for
+            displaying the minimum error for each rho and grid value, 'max' for
+            the maximum error. 0, 1, ... , 7 for displaying error of a specific
+            eigenvalue/energy.
+
+        Raises
+        ------
+        ValueError
+            If selection input is an invalid input.
         """
 
-        rho_max = np.zeros(self.num_rho)
-        error = np.zeros((self.num_n, self.num_rho))
-        n = np.zeros(self.num_n)
+        allowed_selections = [0, 1, 2, 3, 4, 5, 6, 7, "min", "max"]
 
-        for j in range(self.num_n):
+        if selection not in allowed_selections:
+            msg = (f"Selection {selection} not an allowed input. Use one of "
+            f"{allowed_selections}.")
+            raise ValueError(msg)
+
+        # arrays for storing extracted values
+        rho_max = np.zeros(self.num_rho)
+        error   = np.zeros((self.num_grid, self.num_rho))
+        grid    = np.zeros(self.num_grid)
+
+        for j in range(self.num_grid):
             # reading each batch of values per grid size
-            
             for i in range(self.num_rho):
                 # reading each batch of eigenval errors per rho_max
+
+                # generating indices for slicing.
                 start = self.num_eig*(self.num_rho + 1)*j + i*self.num_eig
                 stop  = self.num_eig*(self.num_rho + 1)*j + (i + 1)*self.num_eig
                 
-                idx = np.argmax(self.error[start:stop])
+                if selection == "max":
+                    # finding the index of the max error per rho_max
+                    idx = np.argmax(self.error[start:stop])
+
+                elif selection == "min":
+                    # finding the index of the min error per rho_max
+                    idx = np.argmin(self.error[start:stop])
+
+                else:
+                    # choosing a specific eigenvalue/energy
+                    idx = selection
 
                 error[j, i] = np.log10(self.error[start:stop][idx])
                 rho_max[i]  = self.rho_max[start:stop][idx]
-                n[j] = self.n[start:stop][idx]
+                grid[j]     = self.grid[start:stop][idx]
 
-        fig, ax = plt.subplots()
+        rho_max, grid = np.meshgrid(rho_max, grid)
+        
+        fig, ax = plt.subplots(figsize=(10, 8))
+        plt.contourf(rho_max, grid, error)
+        
+        cbar = plt.colorbar()
+        cbar.set_label(r"$log_{10}$ error", fontsize=40)
+        cbar.ax.tick_params(labelsize=30) 
 
-        rho_max, n = np.meshgrid(rho_max, n)
-        plt.contourf(rho_max, n, error)
-        plt.colorbar()
+        ax.set_xlabel(r"$\rho_{max}$", fontsize=40)
+        ax.set_ylabel(r"grid, n", fontsize=40)
+        ax.tick_params(labelsize=30)
 
         plt.show()
 
 
-    def comparison_plot(self, ax=None):
+    def comparison_plot(self):
         """
-        Generates a standard plot of the different graphs for comparison. Picks
-        the maximum error value per rho_max per grid point value.
+        Generates a standard plot of three different graphs for comparison.
+        Picks the maximum error value per rho_max per grid point value.
         """
 
         max_rhos  = np.zeros(self.num_rho)
         max_error = np.zeros(self.num_rho)
 
-        if ax is None:
-            _, ax = plt.subplots()
+        _, ax = plt.subplots(figsize=(10, 8))
+
+        selected_values = [0, int(self.num_grid/2), self.num_grid-1]
+        colors = ["silver", "gray", "black"]
         
-        col = plt.cm.winter(np.linspace(0, 1, self.num_n))
-        for j in range(self.num_n):
+        # for j in range(self.num_grid):
+        for k, j in enumerate(selected_values):
             # reading each batch of values per grid size
 
             for i in range(self.num_rho):
                 # reading each batch of eigenval errors per rho_max
+
+                # generating indices for slicing.
                 start = self.num_eig*(self.num_rho + 1)*j + i*self.num_eig
                 stop  = self.num_eig*(self.num_rho + 1)*j + (i + 1)*self.num_eig
                 
+                # finding the index of the max error per rho_max
                 idx = np.argmax(self.error[start:stop])
 
+                # extracts the max error and corresponding rho_max
                 max_error[i] = self.error[start:stop][idx]
                 max_rhos[i]  = self.rho_max[start:stop][idx]
+
             
-            if ax is None:
-                ax.plot(max_rhos, max_error, label=100+(j + 1)*10, alpha=0.8, color=col[j])
+            ax.plot(max_rhos, max_error, color=colors[k],
+                label=f"n: {self.grid[start:stop][0]:.0f}", linewidth=3)
 
-            else:
-                ax.plot(max_rhos, max_error)
 
-        if ax is None:
-            ax.set_xlabel("exact")
-            ax.set_ylabel("error")
-            plt.legend()
-            plt.tight_layout(pad=2)
-            plt.grid()
-            plt.show()
 
-    def visualize_frequencies(self):
-
-        fig, ax = plt.subplots()
+        ax.tick_params(labelsize=30)
+        ax.set_xlabel(r"$\rho_{max}$", fontsize=40)
+        ax.set_ylabel("error", fontsize=40)
         
-        filenames = ["eigenvalues_w_0.010000.txt", "eigenvalues_w_0.500000.txt", 
-                    "eigenvalues_w_1.000000.txt", "eigenvalues_w_5.000000.txt"]
-
-        for filename in filenames:
-            
-            with open(filename, "r"):
-                self.__init__(filename)
-                self.comparison_plot(ax)
-        
-        ax.set_xlabel("exact")
-        ax.set_ylabel("error")
-        plt.legend()
-        plt.tight_layout(pad=2)
+        plt.legend(fontsize=35)
         plt.grid()
         plt.show()
 
 
 
 if __name__ == "__main__":
-
-    # filenames = ["eigenvalues_w_0.010000.txt", "eigenvalues_w_0.500000.txt", 
-    #             "eigenvalues_w_1.000000.txt", "eigenvalues_w_5.000000.txt"]
-    
-    # # filenames = ["eigenvalues_tmp.txt"]
-
-    # for filename in filenames:
-    #     q = VisualizeData(filename)
-    #     q.contour_plot()
-    # # q = VisualizeData()
-    # # q.visualize_frequencies()
-
-
-    # Loop over files with .txt ending. Found on https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python
-    # os.chdir("/mydir")
-    filenames = []
-    for file in glob.glob("*.txt"):
-        filenames.append(file)
-    print(filenames)
-
-    for filename in filenames:
-        q = VisualizeData(filename)
+    q = VisualizeData("eigenvalues.txt")
+    # q.contour_plot(selection="min")
+    q.comparison_plot()
     pass
