d9e50bf75ef28214c08ead8090697e9866e88335
Jon Dahl <jonkd@student.matnat.uio.no>
2019-10-03 16:49:51 +0200

    Started implementation of the integrand.

--- project3/src/gauss-legendre-quadrature.cpp
+++ project3/src/gauss-legendre-quadrature.cpp
@@ -1,3 +1,6 @@
+#include <cmath>
+#include <iostream>
+
 
 void gauss_legendre_points(double x1, double x2, double x[], double w[], int n)
 {
@@ -5,6 +8,8 @@ void gauss_legendre_points(double x1, double x2, double x[], double w[], int n)
     double z1, z, xm, xl, pp, p3, p2, p1;
     double const pi = 3.14159265359; 
     double *x_low, *x_high, *w_low, *w_high;
+
+    double const ZERO = std::pow(10, -10);
 
     m  = (n + 1)/2;          // roots are symmetric in the interval
     xm = 0.5 * (x2 + x1);
@@ -17,7 +22,7 @@ void gauss_legendre_points(double x1, double x2, double x[], double w[], int n)
 
     for(i = 1; i <= m; i++)
     {      // loops over desired roots
-      z = cos(pi * (i - 0.25)/(n + 0.5));
+      z = std::cos(pi * (i - 0.25)/(n + 0.5));
 
         /*
         Starting with the above approximation to the ith root
@@ -51,7 +56,7 @@ void gauss_legendre_points(double x1, double x2, double x[], double w[], int n)
 	        z1 = z;
 	        z  = z1 - p1/pp;                   // Newton's method
         }
-        while(fabs(z - z1) > ZERO);
+        while(std::fabs(z - z1) > ZERO);
 
         /* 
 	    ** Scale the root to the desired interval and put in its symmetric
@@ -65,14 +70,59 @@ void gauss_legendre_points(double x1, double x2, double x[], double w[], int n)
     }
 }
 
+double integrand(double x0, double x1, double x2, double x3, double x4, double x5)
+{   /*
+    The integrand to be integrated.
+
+    Parameters
+    ----------
+    many parameters, will write this when things seem to work
+    */
+
+   
+    double res = std::exp(5);
+    // std::cout << N << std::endl;
+    
+    return res;
+}
+
+
 
 void gauss_legendre_quadrature()
 {   
-    int N = 10;
-    double *x = new double [N];
-    double *w = new double [N];
+    int N = 10;                 // grid points
+    double *x = new double [N]; // array of x values
+    double *w = new double [N]; // array of weights
 
+    // integral limits, approx. infinity
+    int a = -10;
+    int b = 10;
+    
     gauss_legendre_points(a, b, x, w, N);
+
+
+    double gauss_sum = 0;
+
+    for (int i0 = 0; i0 < N; i0++)
+    {
+        for (int i1 = 0; i1 < N; i1++)
+        {
+            for (int i2 = 0; i2 < N; i2++)
+            {
+                for (int i3 = 0; i3 < N; i3++)
+                {
+                    for (int i4 = 0; i4 < N; i4++)
+                    {
+                        for (int i5 = 0; i5 < N; i5++)
+                        {
+                            gauss_sum += w[i0]*w[i1]*w[i2]*w[i3]*w[i4]*w[i5]*
+                                integrand(x[i0], x[i1], x[i2], x[i3], x[i4], x[i5]);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
     delete[] x;
@@ -80,9 +130,14 @@ void gauss_legendre_quadrature()
 }
 
 
+
+
 int main()
 {   
+
     gauss_legendre_quadrature();
+
+
 
     return 0;
 }
