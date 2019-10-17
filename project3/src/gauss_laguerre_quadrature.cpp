f85b98df435a4c572d72177ef9b7f7db1f97d38f
Jon Dahl <jonkd@student.matnat.uio.no>
2019-10-17 20:27:46 +0200

    Added write to file capabilities.

--- project3/src/gauss_laguerre_quadrature.cpp
+++ project3/src/gauss_laguerre_quadrature.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
-#include "gauss-laguerre.cpp"
+#include <chrono>
+#include "gauss_laguerre.cpp"
 
 double const pi = 3.14159265359; 
 
@@ -128,67 +129,98 @@ void gauss_laguerre_quadrature()
     using gaussian quadrature with laguerre polynomials for the r-dependence
     and legendre polynomials for the theta- and phi-dependence.
     */
-    
-    int N = 20;         // # of integration points for a single integral.
-    double alpha = 2;   // laguerre assumes a function of the form x^{alpha} exp(-x), and we must specify alpha.
 
-    double *r     = new double[N+1];  // Arrays for the r, theta and phi points.
-    double *theta = new double[N];
-    double *phi   = new double[N];
+    // generating data file
+    std::ofstream laguerre_data_file;
+    laguerre_data_file.open("laguerre_data.txt", std::ios_base::app);
 
-    double *w_r     = new double[N+1]; // Arrays for the r, theta and phi weights.
-    double *w_theta = new double[N];
-    double *w_phi   = new double[N];
+    // writing title to file
+    laguerre_data_file << std::setw(20) << "N" << std::setw(20) << "error";
+    laguerre_data_file << std::setw(20) << "calculated";
+    laguerre_data_file << std::setw(20) << "exact";
+    laguerre_data_file << std::setw(20) << "comp time (s)" << std::endl;
 
-    // Finding the weights and points for integration.
-    gauss_laguerre(r, w_r, N+1, alpha);     // possibly change to N
-    gauss_legendre_points(0, pi, theta, w_theta, N);
-    gauss_legendre_points(0, 2*pi, phi, w_phi, N);
+    // int N = 20;         // # of integration points for a single integral.
+    int N_end = 40;
 
+    for (int N = 1; N <= N_end; N = N + 2)
+    {   // loops over grid values
 
-    double gauss_sum = 0;
-
-    // The actual integral is approximated with a sum.
-    for (int i0 = 1; i0 < N+1; i0++)
-    {   
-        std::cout << "outer loop: " << i0 << " of " << N << std::endl;
+        // starting timer
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
         
-        for (int i1 = 1; i1 < N+1; i1++)
-        {
-            for (int i2 = 0; i2 < N; i2++)
+        double alpha = 2;   // laguerre assumes a function of the form x^{alpha} exp(-x), and we must specify alpha.
+
+        double *r     = new double[N+1];  // Arrays for the r, theta and phi points.
+        double *theta = new double[N];
+        double *phi   = new double[N];
+
+        double *w_r     = new double[N+1]; // Arrays for the r, theta and phi weights.
+        double *w_theta = new double[N];
+        double *w_phi   = new double[N];
+
+        // Finding the weights and points for integration.
+        gauss_laguerre(r, w_r, N, alpha);     // possibly change to N
+        gauss_legendre_points(0, pi, theta, w_theta, N);
+        gauss_legendre_points(0, 2*pi, phi, w_phi, N);
+
+
+        double integral_sum = 0;
+
+        // The actual integral is approximated with a sum.
+        for (int i0 = 1; i0 < N+1; i0++)
+        {   
+            std::cout << "outer loop: " << i0 << " of " << N << std::endl;
+            
+            for (int i1 = 1; i1 < N+1; i1++)
             {
-                for (int i3 = 0; i3 < N; i3++)
+                for (int i2 = 0; i2 < N; i2++)
                 {
-                    for (int i4 = 0; i4 < N; i4++)
+                    for (int i3 = 0; i3 < N; i3++)
                     {
-                        for (int i5 = 0; i5 < N; i5++)
+                        for (int i4 = 0; i4 < N; i4++)
                         {
-                            // Multiplying the weights with the integrand.
-                            gauss_sum += w_r[i0]*w_r[i1]*w_theta[i2]*w_theta[i3]*w_phi[i4]*w_phi[i5]
-                                *integrand(r[i0], r[i1], theta[i2], theta[i3], phi[i4], phi[i5])
-                                *std::sin(theta[i2])*std::sin(theta[i3]);
+                            for (int i5 = 0; i5 < N; i5++)
+                            {
+                                // Multiplying the weights with the integrand.
+                                integral_sum += w_r[i0]*w_r[i1]*w_theta[i2]*w_theta[i3]*w_phi[i4]*w_phi[i5]
+                                    *integrand(r[i0], r[i1], theta[i2], theta[i3], phi[i4], phi[i5])
+                                    *std::sin(theta[i2])*std::sin(theta[i3]);
+                            }
                         }
                     }
                 }
             }
         }
+
+        // factor from change of variables
+        integral_sum /= std::pow( (2*2), 5);
+
+        // ending timer
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+        std::chrono::duration<double> comp_time = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1);
+
+        double exact = 5*pi*pi/(16*16);
+        double error = std::fabs(integral_sum - exact);
+
+        std::cout << "\ncalculated: " << integral_sum << std::endl;
+        std::cout << "correct answer: " << exact << std::endl;
+        std::cout << "error: " << error << std::endl;
+        std::cout << N << " of " << N_end << "\n" << std::endl;
+
+        laguerre_data_file << std::setw(20) << N << std::setw(20) << error;
+        laguerre_data_file << std::setw(20) << integral_sum;
+        laguerre_data_file << std::setw(20) << exact;
+        laguerre_data_file << std::setw(20) << comp_time.count() << std::endl;
+
+        delete[] r;
+        delete[] theta;
+        delete[] phi;
+
+        delete[] w_r;
+        delete[] w_theta;
+        delete[] w_phi;
     }
-
-    // factor from change of variables
-    gauss_sum /= std::pow( (2*2), 5);
-
-    std::cout << "calculated: " << gauss_sum << std::endl;
-    std::cout << "correct answer: " << 5*pi*pi/(16*16) << std::endl;
-    std::cout << "error: " << std::fabs(gauss_sum - 5*pi*pi/(16*16)) << std::endl;
-    std::cout << "factor: " << (5*pi*pi/(16*16))/gauss_sum << std::endl;
-
-    delete[] r;
-    delete[] theta;
-    delete[] phi;
-
-    delete[] w_r;
-    delete[] w_theta;
-    delete[] w_phi;
 }
 
 
