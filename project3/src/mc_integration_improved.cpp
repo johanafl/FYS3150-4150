039e9803dd5d5f1a0cf5ed0db91c7c111530a9e0
Jon Dahl <jonkd@student.matnat.uio.no>
2019-10-18 19:38:54 +0200

    Merged into a single loop. Other variance business.

--- project3/src/mc_integration_improved.cpp
+++ project3/src/mc_integration_improved.cpp
@@ -62,8 +62,11 @@ int mc_integration()
         The seed is the system time in seconds from UNIX epoch.
     */
 
-    int N = 10;       // number of iterations = N**6
-    float lambda = 1; // For the exp. distribution function.
+    int N = 1e7;       // number of iterations
+    
+    // if we choose lambda = 1, we need only one exp distribution
+    // if lambda = 2*alpha, we need another distribution with lambda = 4*alpha
+    float lambda = 1;
 
     // generate engine with pseudo-random seed taken from system time
     time_t seed;
@@ -75,46 +78,60 @@ int mc_integration()
     std::uniform_real_distribution<double> uniform_phi(0, 2*pi);
     std::exponential_distribution<double> exp_dist(lambda);
 
-
     double integral_sum = 0;
     double integral_sum_square = 0;
-    double N5 = std::pow(N, 5); // pre-calculated for the inner sum
+    double N5 = std::pow(N, 5);         // pre-calculated for the inner sum
+    double integrand_tmp;               // temporary value for squaring the integrand so we don't have to call the function twice   
+    int counter = 0;
+    
+    
+    
+    for (int i = 0; i < N; i++)
+    {
 
-    for (int i0 = 0; i0 < N; i0++)
-    {   // first loop is for displaying progress info without an if statement
-        
-        std::cout << "outer loop: " << i0 << " of " << N-1 << std::endl;
-
-        for (int i1 = 0; i1 < N5; i1++)
+        if ( counter%(N/10) == 0 )
         {
-            // drawing random numbers from the distributions
-            double r1 = exp_dist(engine); double R1 = exp_dist(engine);
-            double r2 = exp_dist(engine); double R2 = exp_dist(engine);
-            double theta1 = uniform_theta(engine); double Theta1 = uniform_theta(engine);
-            double theta2 = uniform_theta(engine); double Theta2 = uniform_theta(engine);
-            double phi1 = uniform_phi(engine); double Phi1 = uniform_phi(engine);
-            double phi2 = uniform_phi(engine); double Phi2 = uniform_phi(engine);
-
-            // adding to the integrand sum
-            integral_sum += integrand(r1, r2, theta1, theta2, phi1, phi2)
-                *r1*r1*r2*r2*std::sin(theta1)*std::sin(theta2);
-
-            integral_sum_square += integrand(R1,R2,Theta1,Theta2,Phi1,Phi2)*integrand(R1,R2,Theta1,Theta2,Phi1,Phi2)
-                *R1*R1*R2*R2*std::sin(Theta1)*std::sin(Theta2)*R1*R1*R2*R2*std::sin(Theta1)*std::sin(Theta2);        
+            std::cout << "iteration: " << i << " of " << N-1 << std::endl;
         }
+        // drawing random numbers from the distributions
+        // drawing twice for each variable for calculating the variance
+        double r1 = exp_dist(engine); double R1 = exp_dist(engine);
+        double r2 = exp_dist(engine); double R2 = exp_dist(engine);
+        double theta1 = uniform_theta(engine); double Theta1 = uniform_theta(engine);
+        double theta2 = uniform_theta(engine); double Theta2 = uniform_theta(engine);
+        double phi1 = uniform_phi(engine); double Phi1 = uniform_phi(engine);
+        double phi2 = uniform_phi(engine); double Phi2 = uniform_phi(engine);
+
+        // adding to the integrand sum
+        integral_sum += integrand(r1, r2, theta1, theta2, phi1, phi2)
+            *r1*r1*r2*r2*std::sin(theta1)*std::sin(theta2);
+        
+        // adding to the f(x)**2 sum for the variance
+        integrand_tmp = integrand(R1, R2, Theta1, Theta2, Phi1, Phi2);
+        integral_sum_square += integrand_tmp*integrand_tmp*R1*R2*R1*R2
+            *std::sin(Theta1)*std::sin(Theta2);
+
+        counter++;
     }
 
-    integral_sum /= std::pow(N, 6);        // number of samples
-    integral_sum_square /= std::pow(N, 6);        // number of samples
-    std::cout << "\nvarians: " << integral_sum_square - integral_sum*integral_sum << std::endl;
     
-    integral_sum *= 4*std::pow(pi, 4);     // theta, phi interval
-    integral_sum /= std::pow((2*2), 5);    // (2*alpha)**5
+    integral_sum /= N;                 // number of samples
+    integral_sum_square /= N;          // number of samples
+    
+    integral_sum_square /= std::pow( (2*2), 10 );   // (2*alpha)**10
+    integral_sum /= std::pow( (2*2), 5);            // (2*alpha)**5
 
+    double variance = (integral_sum_square - integral_sum*integral_sum)*4*std::pow(pi, 4);
+    
+    std::cout << "\nvariance: " << variance << std::endl;
+    std::cout << "std: " << std::sqrt(variance) << std::endl;
+
+    integral_sum *= 4*std::pow(pi, 4);     // theta, phi interval
+    
     std::cout << "calculated: " << integral_sum << std::endl;
     std::cout << "correct answer: " << 5*pi*pi/(16*16) << std::endl;
     std::cout << "error: " << std::fabs(integral_sum - 5*pi*pi/(16*16)) << std::endl;
-    // std::cout << "iterations: " << std::pow(N, 6) << std::endl;
+    std::cout << "iterations: " << N << std::endl;
 
     return seed;
 }
