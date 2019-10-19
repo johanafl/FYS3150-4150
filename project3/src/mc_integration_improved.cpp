1fcb872d599abe77349c791c8a0b39708b47a7e6
Jon Dahl <jonkd@student.matnat.uio.no>
2019-10-19 22:50:58 +0200

    Added write to file.

--- project3/src/mc_integration_improved.cpp
+++ project3/src/mc_integration_improved.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <random>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+
 double const pi = 3.14159265359; 
 
 
@@ -51,19 +55,34 @@ double integrand(double r1, double r2, double theta1, double theta2, double phi1
 }
 
 
-int mc_integration()
+void mc_integration(int N_start, int N_end, int dN)
 {
     /*
     Monte Carlo integration of the function exp(-2*2*(r1 + r2))/|r1 - r2|.
 
-    Returns
-    -------
-    seed : int
-        The seed is the system time in seconds from UNIX epoch.
+    Parameters
+    ----------
+    N_start : int
+        Start iteration value.
+
+    N_end : int
+        End iteration value.
+
+    dN : int
+        Iteration step size.
     */
 
-    int N = 1e7;       // number of iterations
-    
+    // generating data file
+    std::ofstream mc_improved_data_file;
+    mc_improved_data_file.open("data_files/mc_improved_data.txt", std::ios_base::app);
+
+    // writing title to file
+    mc_improved_data_file << std::setw(20) << "N" << std::setw(20) << "error";
+    mc_improved_data_file << std::setw(20) << "calculated";
+    mc_improved_data_file << std::setw(20) << "exact";
+    mc_improved_data_file << std::setw(20) << "comp time (s)";
+    mc_improved_data_file << std::setw(20) << "variance" << std::endl;
+
     // if we choose lambda = 1, we need only one exp distribution
     // if lambda = 2*alpha, we need another distribution with lambda = 4*alpha
     float lambda = 1;
@@ -79,65 +98,103 @@ int mc_integration()
     std::exponential_distribution<double> exp_dist(lambda);
 
     double integral_sum = 0;
+    double average_sum;
+    double average_time;
     double integral_sum_square = 0;
     double integrand_tmp;               // temporary value for squaring the integrand so we don't have to call the function twice
-    int counter = 0;
-    
-    
-    
-    for (int i = 0; i < N; i++)
-    {
+    int average_runs = 10;      // number of iterations for the average
+    double variance;
+    double exact = 5*pi*pi/(16*16);
 
-        if ( counter%(N/10) == 0 )
-        {
-            std::cout << "iteration: " << i << " of " << N-1 << std::endl;
+    double r1;     double R1;
+    double r2;     double R2;
+    double theta1; double Theta1;
+    double theta2; double Theta2;
+    double phi1;   double Phi1;
+    double phi2;   double Phi2;
+
+    
+    for (int N = N_start; N <= N_end; N += dN)
+    {   // loops over MC iterations
+
+        // resetting the averages
+        average_sum = 0;
+        average_time = 0;
+
+        for (int _ = 0; _ < average_runs; _++)
+        {   // averaging to get results without too many statistical flukes
+
+            // starting timer
+            std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+
+            for (int i = 0; i < N; i++)
+            {
+                // drawing random numbers from the distributions
+                // drawing twice for each variable for calculating the variance
+                r1 = exp_dist(engine);          R1 = exp_dist(engine);
+                r2 = exp_dist(engine);          R2 = exp_dist(engine);
+                theta1 = uniform_theta(engine); Theta1 = uniform_theta(engine);
+                theta2 = uniform_theta(engine); Theta2 = uniform_theta(engine);
+                phi1 = uniform_phi(engine);     Phi1 = uniform_phi(engine);
+                phi2 = uniform_phi(engine);     Phi2 = uniform_phi(engine);
+
+                // adding to the integrand sum
+                integral_sum += integrand(r1, r2, theta1, theta2, phi1, phi2)
+                    *r1*r1*r2*r2*std::sin(theta1)*std::sin(theta2);
+                
+                // adding to the f(x)**2 sum for the variance
+                integrand_tmp = integrand(R1, R2, Theta1, Theta2, Phi1, Phi2);
+                integral_sum_square += integrand_tmp*integrand_tmp*R1*R2*R1*R2
+                    *std::sin(Theta1)*std::sin(Theta2);
+            }
+
+            integral_sum /= N;                  // number of samples
+            integral_sum_square /= N;          // number of samples
+            integral_sum_square /= std::pow( (2*2), 10 );   // (2*alpha)**10
+            integral_sum /= std::pow( (2*2), 5);            // (2*alpha)**5
+            variance = (integral_sum_square - integral_sum*integral_sum)*4*std::pow(pi, 4);
+            integral_sum *= 4*std::pow(pi, 4);     // theta, phi interval
+
+            average_sum += integral_sum;
+
+            // ending timer
+            std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+            std::chrono::duration<double> comp_time  = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1);
+            average_time += comp_time.count();
+            
         }
-        // drawing random numbers from the distributions
-        // drawing twice for each variable for calculating the variance
-        double r1 = exp_dist(engine); double R1 = exp_dist(engine);
-        double r2 = exp_dist(engine); double R2 = exp_dist(engine);
-        double theta1 = uniform_theta(engine); double Theta1 = uniform_theta(engine);
-        double theta2 = uniform_theta(engine); double Theta2 = uniform_theta(engine);
-        double phi1 = uniform_phi(engine); double Phi1 = uniform_phi(engine);
-        double phi2 = uniform_phi(engine); double Phi2 = uniform_phi(engine);
+        average_sum  /= average_runs;
+        average_time /= average_runs;
 
-        // adding to the integrand sum
-        integral_sum += integrand(r1, r2, theta1, theta2, phi1, phi2)
-            *r1*r1*r2*r2*std::sin(theta1)*std::sin(theta2);
-        
-        // adding to the f(x)**2 sum for the variance
-        integrand_tmp = integrand(R1, R2, Theta1, Theta2, Phi1, Phi2);
-        integral_sum_square += integrand_tmp*integrand_tmp*R1*R2*R1*R2
-            *std::sin(Theta1)*std::sin(Theta2);
+        double error = std::fabs(average_sum - exact);
 
-        counter++;
+        std::cout << "\nvariance: " << variance << std::endl;
+        std::cout << "std: " << std::sqrt(variance) << std::endl;
+        std::cout << "calculated: " << average_sum << std::endl;
+        std::cout << "correct answer: " << exact << std::endl;
+        std::cout << "error: " << error << std::endl;
+        std::cout << "iterations: " << N << std::endl;
+
+        // writing calculation data to file
+        mc_improved_data_file << std::setw(20) << N << std::setw(20) << error;
+        mc_improved_data_file << std::setw(20) << average_sum;
+        mc_improved_data_file << std::setw(20) << exact;
+        mc_improved_data_file << std::setw(20) << average_time;
+        mc_improved_data_file << std::setw(20) << variance << std::endl;
     }
 
-    
-    integral_sum /= N;                 // number of samples
-    integral_sum_square /= N;          // number of samples
-    
-    integral_sum_square /= std::pow( (2*2), 10 );   // (2*alpha)**10
-    integral_sum /= std::pow( (2*2), 5);            // (2*alpha)**5
-
-    double variance = (integral_sum_square - integral_sum*integral_sum)*4*std::pow(pi, 4);
-    
-    std::cout << "\nvariance: " << variance << std::endl;
-    std::cout << "std: " << std::sqrt(variance) << std::endl;
-
-    integral_sum *= 4*std::pow(pi, 4);     // theta, phi interval
-    
-    std::cout << "calculated: " << integral_sum << std::endl;
-    std::cout << "correct answer: " << 5*pi*pi/(16*16) << std::endl;
-    std::cout << "error: " << std::fabs(integral_sum - 5*pi*pi/(16*16)) << std::endl;
-    std::cout << "iterations: " << N << std::endl;
-
-    return seed;
+    mc_improved_data_file.close();
 }
 
 
 int main()
 {
-    mc_integration();
-    return 1;
+
+    int N_end   = 1e7;
+    int dN      = 1e5;
+    int N_start = dN;
+    
+    mc_integration(N_start, N_end, dN);
+    
+    return 0;
 }
