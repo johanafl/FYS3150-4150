1cf271c84e51ad81bfe6686dd908cbedfec3a680
Jon Dahl <jonkd@student.matnat.uio.no>
2019-10-25 18:19:28 +0200

    Padding trouble forces us to make a matrix class.

--- project4/src/energy_solver.cpp
+++ project4/src/energy_solver.cpp
@@ -6,82 +6,125 @@
 #include <chrono>
 #include <cmath>
 #include <armadillo>
-double const pi = 3.14159265359; 
+double const pi = 3.14159265359;
+
+
 
 double** initial_spin(int n)
-{
-    time_t seed;
-    time(&seed);
+{   /*
+    Creates a n x n matrix where last row and last column are copies of the
+    first row and first column (padding).
+
+    Parameters
+    ----------
+    n : int
+        Grid dimension.
+
+    Returns
+    -------
+    spin : double**
+        Matrix of random spin values (+- 1).
+    */
+    
+    int seed = 1337;
+    // time_t seed;
+    // time(&seed);
     std::mt19937 engine(seed);
-    // generating distribution
     std::uniform_int_distribution<int> uniform(0, 1);
 
-    double** spin = new double*[n];
+    double** spin = new double*[n+2];
 
-    for (int i=0; i<n; i++)
-    {
-        spin[i] = new double[n];
-        for (int j=0; j<n; j++)
-        {
-            double spin_val = 2 * uniform(engine) - 1;
+    for (int i = 0; i < n + 2; i++)
+    {   // creates an array for every element in the spin array
+        // n + 1 here to create the last column, even though we overwrite the
+        // values later
+        
+        spin[i] = new double[n+2];
+        
+        for (int j = 1; j < n + 1; j++)
+        {   // creates +- 1 values and inserts into array
+            
+            double spin_val = 2*uniform(engine) - 1;  // +- 1
             spin[i][j] = spin_val;
         }
     }
+
+    std::cout << &spin[1][1] << std::endl;
+
+    // for (int i = 0; i < n; i++)
+    // {   // copies first row and first column into the last row and last
+    //     // i < n instead of i < n + 1 drops the last unused element.
+        
+    //     &spin[n][i] = &spin[0][i];
+    //     &spin[i][n] = &spin[i][0];
+    // }
 
     return spin;
 }
 
 double total_energy_and_magnetization(double** spin, int n, double& tot_magnet)
-{
-    /*
-    Computing from upper left in matrix and down to the right. 
+{   /*
+    Computing from upper left in matrix and down to the right.
+
+    Parameters
+    ----------
+    spin : double**
+        n x n matrix represented by a double pointer.
+    
+    n : int
+        Grid dimension.
+
+    tot_magnet : double refernce
+        Total magnetic moment.
     */
+
     double tot_energy = 0;
     
-    for (int i=0; i<n-1; i++)
-    {
-        for (int j=0; j<n-1; j++)
-        {
-            double spin_now   = spin[i][j];
-            double spin_down  = spin[i+1][j];
-            double spin_right = spin[i][j+1];
+    for (int i = 0; i < n; i++)
+    {   // looping over rows
+        
+        for (int j = 0; j < n; j++)
+        {   // looping over columns
 
-            tot_energy -= spin_now*(spin_right + spin_down);
-            tot_magnet += spin_now;
+            double spin_here  = spin[i][j];     // spin of the current indices
+            double spin_below = spin[i+1][j];   // below
+            double spin_right = spin[i][j+1];   // right
+
+            tot_energy -= spin_here*(spin_right + spin_below);
+            tot_magnet += spin_here;
         }
     }
     
-    for (int j=0; j<n-1; j++)
-    {
-        double spin_now   = spin[n-1][j];
-        double spin_down  = spin[0][j];
-        double spin_right = spin[n-1][j+1];
-
-        tot_energy -= spin_now*(spin_right + spin_down);
-        tot_magnet += spin_now;
-    }
-
-    for (int i=0; i<n-1; i++)
-    {
-        double spin_now   = spin[i][n-1];
-        double spin_down  = spin[i+1][n-1];
-        double spin_right = spin[i][0];
-
-        tot_energy -= spin_now*(spin_right + spin_down);
-        tot_magnet += spin_now;
-    }
-
-    tot_energy += spin[n-1][n-1]*(spin[n-1][0] + spin[0][n-1]);
-    tot_magnet += spin[n-1][n-1];
-
+ 
     return tot_energy;
 }
 
-void print_matrix(double** matrix, int n)
-{
-    for (int i=0; i<n; i++)
+void print_matrix(double** matrix, int n, bool pad)
+{   /*
+    Prints matrix.
+
+    Parameters
+    ----------
+    matrix : double**
+        n x n matrix represented by a double pointer (n+1 x n+1 with pad).
+
+    n : int
+        Grid dimension.
+
+    pad : bool
+        Option to toggle print of padding on/off.
+    */
+
+    int m = n;
+
+    if (pad)
+    {   // for the extra row and column
+        m = n + 1;
+    }
+
+    for (int i = 0; i < m; i++)
     {
-        for (int j=0; j<n; j++)
+        for (int j = 0; j < m; j++)
         {
             std::cout << std::setw(4) << matrix[i][j];
         }
@@ -89,86 +132,61 @@ void print_matrix(double** matrix, int n)
     }
 }
 
-double metropolis_flap(double** spin_mat, int row, int col, int n)
-{   
-    /*
-    This is wrong!!!!!! But now it is also very different...
+void metropolis_flap(double** spin, double& tot_energy, double& tot_magnet,
+        int row, int col, double metro_con, double T)
+{   /*
+    
+    Parameters
+    ----------
+    spin : double**
+        Matrix of spin values.
+    
+    tot_energy : double reference
+        Total energy of all the spins.
+
+    tot_magnet : double reference
+        Total magnetic moment of the system.
+
+    row : int
+        A randomly chosen row index.
+
+    col : int
+        A randomly chosen column index.
+
+    metro_con : double
+        Random variable drawn from a uniform distribution on the interval
+        [0, 1). Metropolis condition.
     */
+
+    std::cout << "row: " << row << " col: " << col << std::endl;
     
-    double spin_now   = spin[row][col];
-    
-    // PROV AA DROPPE IF_TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!v
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    
+    double spin_here  = -1*spin[row][col];  // flippity flap
     double spin_left  = spin[row][col-1];
-    double spin_up    = spin[row-1][col];
+    double spin_above = spin[row-1][col];
     double spin_right = spin[row][col+1];
-    double spin_down  = spin[row+1][col];
+    double spin_below = spin[row+1][col];
+
+    double delta_energy = 2*spin_here*(spin_above + spin_below + spin_right + spin_left);
+
+    // if (delta_energy <= 0)
+    // {   // accept new energy if difference is negative
+        
+    //     spin[row][col] *= -1;
+    //     tot_energy += delta_energy;
+    //     tot_magnet += spin_here;
+    // }
     
-    if (row == n-1)
-    {
-        spin_down  = spin[0][col];
-    }
-    if (row == 0)
-    {
-        spin_up    = spin[n-1][col];
-    }
-    if (col == n-1)
-    {
-        spin_right = spin[row][0];
-    }
-    if (col == 0)
-    {
-        spin_left  = spin[row][n-1];
-    }
+    // else if ( (delta_energy > 0) and (metro_con < std::exp(delta_energy/T)) )
+    // {   // checks if energy difference is positive and the metropolis condition true
+        
+    //     spin[row][col] *= -1;
+    //     tot_energy += delta_energy;
+    //     tot_magnet += spin_here;
+    // }
+    
 
-    double r            = 0;    // NB!! THIS NEEDS TO BE A RANDOM NUMBER IN THE INTERVAL [0, 1]
-    double energy       = spin_now*(spin_up + spin_down + spin_right + spin_left);
-    double delta_energy = 2*(-energy)
-
-    if r < cmath::exp(delta_energy)
-    {
-        spin_now *= -1
-    }
-
-    return delta_energy;
 }
 
-// double enegy_from_flap(double** spin_mat, int row, int col, int n)
-// {   
-//     /*This is wrong!!!!!!*/
-//     double spin_now   = spin[row][col]*(-1);
-//     // PROV AA DROPPE IF_TEST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!v
-//     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//     double spin_left  = spin[row][col-1];
-//     double spin_up    = spin[row-1][col];
-//     double spin_right = spin[row][col+1];
-//     double spin_down  = spin[row+1][col];
-//     if (row == n-1)
-//     {
-//         spin_down  = spin[0][col];
-//     }
-//     if (row == 0)
-//     {
-//         spin_up    = spin[n-1][col];
-//     }
-//     if (col == n-1)
-//     {
-//         spin_right = spin[row][0];
-//     }
-//     if (col == 0)
-//     {
-//         spin_left  = spin[row][n-1];
-//     }
-
-//     double delta_energy = spin_now*(spin_up + spin_down + spin_right + spin_left);
-
-//     return delta_energy;
-// }
 
 // void mc_integration(arma::mat spin_mat)
 // {
@@ -178,6 +196,8 @@ double metropolis_flap(double** spin_mat, int row, int col, int n)
 
 //     }
 // }
+
+
 
 int main()
 {   
@@ -191,23 +211,50 @@ int main()
     // MPI_Finalize();
 
     int n = 3;
+    double temperature = 1;
+    
+    int seed = 1337;
+    // time_t seed;
+    // time(&seed);
+    std::mt19937 engine(seed);
+    std::uniform_int_distribution<int> uniform_discrete(0, n - 1);
+    std::uniform_real_distribution<double> uniform_continuous(0, 1);
+
+
+
+
     double** init_spin = initial_spin(n);
 
+    // double magnetization;
+    // double energy = total_energy_and_magnetization(init_spin, n, magnetization);
 
-    double magnetization;
-    double energy = total_energy_and_magnetization(init_spin, n, magnetization);
+    // print_matrix(init_spin, n, true);
 
-    print_matrix(init_spin, n);
+    // std::cout << std::endl;
+    // std::cout << "tot M: " << magnetization << ", tot_E: " << energy << "\n" << std::endl;
+    
+    // for (int i = 0; i < 10; i++)
+    // {   
+    //     int row = uniform_discrete(engine);
+    //     int col = uniform_discrete(engine);
+    //     double metro_cond = uniform_continuous(engine);
+        
+    //     metropolis_flap(init_spin, energy, magnetization, row, col, metro_cond, temperature);
+    // }
 
-    std::cout << std::endl;
-    std::cout << "tot M: " << magnetization << ", tot_E: " << energy << std::endl;
+
+    // print_matrix(init_spin, n, true);
+
+    // std::cout << std::endl;
+    // std::cout << "tot M: " << magnetization << ", tot_E: " << energy << std::endl;
 
     // HUSK DELETE!!!!!!!!!!
-    for (int i=0; i<n; i++)
+    for (int i = 0; i < n + 1; i++)
     {
         delete[] init_spin[i];
     }
     delete[] init_spin;
+
 
     return 0;
 }
