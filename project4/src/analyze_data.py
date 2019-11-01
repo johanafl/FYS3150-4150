4541a79ef721d99c752f615a5714155e8ec4e075
Mats Ola Sand <matsolasand@outlook.com>
2019-11-01 15:10:36 +0100

    added heat capacity and susceptibility to the plots for different temperatures.

--- project4/src/analyze_data.py
+++ project4/src/analyze_data.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def analyse_energy_const_temp(energy):
-    plt.plot(energy, label="Total energy, [?]")
+    plt.plot(energy[1], label="Total energy, [?]")
     plt.xlabel("MC iterations")
     plt.ylabel("Energy, [?]")
     plt.legend(loc="best")
     plt.show()
 
 def analyse_magnet_const_temp(magnet):
-    plt.plot(magnet, label="Total magnetization, [?]")
+    plt.plot(magnet[2], label="Total magnetization, [?]")
     plt.xlabel("MC iterations")
     plt.ylabel("Magnetization, [?]")
     plt.legend(loc="best")
@@ -44,6 +44,28 @@ def analyse_magnet(temp, avr_magnet, avr_magnet_square):
     plt.show()
 
 def analayz_tampratar(ax, filename, properties=[]):
+    """
+    Function for plotting the different properties of the crystal against 
+    temperature, i.e., plotting average energy, average (energy^2), ...
+
+    Parameters
+    ----------
+
+    ax : axis object
+
+    filename : string
+        name of the data file
+
+    properties : bool, list_like(?)
+        List of boolian values for triggering the plot for the different
+        physical properties; the True value needs to be in 1st, 2nd, 3rd, 4th,
+        5th, 6th or 7th place in the list in order to see plot of mean energy,
+        mean energy^2, mean magnetization, mean magnetization^2, mean
+        absolute magnetization, heat capacity or susceptibility respectively.
+
+        OBS!!
+        Don't send in more than 1 True value!!!
+    """
     data = np.loadtxt(filename)
 
     T         = data[:, 0]
@@ -52,41 +74,59 @@ def analayz_tampratar(ax, filename, properties=[]):
     M         = data[:, 3]
     M_squared = data[:, 4]
     M_absolut = data[:, 5]
-
+    
     if properties[0]:
+        ax.plot(T, E)
         ax.set_xlabel("Temperature")
         ax.set_ylabel("Energy")
         ax.set_title("Nice title here")
-        ax.plot(T, E);
     
     if properties[1]:
+        ax.plot(T, E_squared)
         ax.set_xlabel("Temperature")
         ax.set_ylabel("Energy squared")
         ax.set_title("Nice title here")
-        ax.plot(T, E_squared);
     
     if properties[2]:
+        ax.plot(T, M)
         ax.set_xlabel("Temperature")
         ax.set_ylabel("Magnetization")
         ax.set_title("Nice title here")
-        ax.plot(T, M);
     
     if properties[3]:
+        ax.plot(T, M_squared)
         ax.set_xlabel("Temperature")
         ax.set_ylabel("Magnetization squared")
         ax.set_title("Nice title here")
-        ax.plot(T, M_squared);
 
     if properties[4]:
+        ax.plot(T, M_absolut)
         ax.set_xlabel("Temperature")
         ax.set_ylabel("Absolute magnetization")
         ax.set_title("Nice title here")
-        ax.plot(T, M_absolut);
+    
+    if properties[5]:
+        scaling_factor = 1
+        heat_capasity  = (E_squared - E**2)/scaling_factor
+
+        ax.plot(T, heat_capasity)
+        ax.set_xlabel("Temperature")
+        ax.set_ylabel("Heat capacity")
+        ax.set_title("Nice title here")
+    
+    if properties[6]:
+        scaling_factor = 1
+        susceptibility = (M_squared - M**2)/scaling_factor
+
+        ax.plot(T, susceptibility)
+        ax.set_xlabel("Temperature")
+        ax.set_ylabel("Susceptibility")
+        ax.set_title("Nice title here")
 
 if __name__ == "__main__":
-    filename_energy = "data_files/E_data.txt"
-    filename_magnet = "data_files/M_data.txt"
-    filename_props  = "data_files/bajsa_temperaturen"
+    filename_energy = "data_files/E_convergence_data.txt"
+    filename_magnet = "data_files/M_convergence_data.txt"
+    filename_props  = "data_files/ising_model_data.txt"
 
     energy = np.loadtxt(filename_energy)
     magnet = np.loadtxt(filename_magnet)
@@ -95,5 +135,8 @@ if __name__ == "__main__":
     analyse_energy_const_temp(energy)
     analyse_magnet_const_temp(magnet)
 
-    analayz_tampratar(ax, filename_props, [True, False, False, False, False])
+    # fig, ax = plt.subplots()
+
+    # analayz_tampratar(ax, filename_props, [True, False, False, False, False, False, False])
+    # plt.show()
     pass
