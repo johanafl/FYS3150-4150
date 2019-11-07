daa0836c8a047254eec74c43a040d64592fb3060
Mats Ola Sand <matsolasand@outlook.com>
2019-11-07 13:30:54 +0100

    implemented test_if_calculated_energy_match_analytical_answer_for_dim_2; will now be tested

--- project4/src/test_energy_solver.cpp
+++ project4/src/test_energy_solver.cpp
@@ -1,18 +1,31 @@
 #include "energy_solver.cpp"
 
 // TEST_CASE("test_if_energy_for_four_given_configurations_match_analytic_answer_for_dim_2")
-void test_if_energy_for_four_given_configurations_match_analytic_answer_for_dim_2()
+void test_if_calculated_energy_match_analytical_answer_for_dim_2()
 {
-    double init1[4] = {1, 1, 1, 1};
-    double init2[4] = {1, -1, -1, 1};
-    double init3[4] = {1, 1, -1, 1};
-    double init4[4] = {-1, -1, -1, -1};
+    double total_energy;
+    double total_magnetization;
+    int n = 2;
+    double spin1[4] = {1, 1, 1, 1};
+    double spin2[4] = {1, -1, -1, 1};
+    double spin3[4] = {1, 1, -1, 1};
+    double spin4[4] = {-1, -1, -1, -1};
+    double energy[4] = {-8, 8, 0, -8};
 
     // mat(dimension, configuration)
-    CircularMatrix mat1(2, init1);
-    CircularMatrix mat2(2, init2);
-    CircularMatrix mat3(2, init3);
-    CircularMatrix mat4(2, init4);
+    CircularMatrix mat1(2, spin1);
+    CircularMatrix mat2(2, spin2);
+    CircularMatrix mat3(2, spin3);
+    CircularMatrix mat4(2, spin4);
+
+    REQUIRE(IsingModel total_energy_and_magnetization(spin1, n,
+            total_energy, total_magnetization)) == -8;
+    REQUIRE(IsingModel total_energy_and_magnetization(spin2, n,
+            total_energy, total_magnetization)) == 8;
+    REQUIRE(IsingModel total_energy_and_magnetization(spin3, n,
+            total_energy, total_magnetization)) == 0;
+    REQUIRE(IsingModel total_energy_and_magnetization(spin4, n,
+            total_energy, total_magnetization)) == -8;
 
     // mat1.print();
     // std::cout << std::endl;
