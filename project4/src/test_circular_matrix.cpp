c85d271398d18c355df850d8e971383b89638755
Mats Ola Sand <matsolasand@outlook.com>
2019-11-07 12:53:57 +0100

    initial start initiated initially

--- project4/src/test_circular_matrix.cpp
+++ project4/src/test_circular_matrix.cpp
@@ -18,31 +18,6 @@ void test_print()
     // std::cout << q1(0, 8, true) << std::endl;
 }
 
-// TEST_CASE("test_if_energy_for_four_given_configurations_match_analytic_answer_for_dim_2")
-void test_if_energy_for_four_given_configurations_match_analytic_answer_for_dim_2()
-{
-    double init1[4] = {1, 1, 1, 1};
-    double init2[4] = {1, -1, -1, 1};
-    double init3[4] = {1, 1, -1, 1};
-    double init4[4] = {-1, -1, -1, -1};
-
-    // mat(dimension, configuration)
-    CircularMatrix mat1(2, init1);
-    CircularMatrix mat2(2, init2);
-    CircularMatrix mat3(2, init3);
-    CircularMatrix mat4(2, init4);
-
-    // mat1.print();
-    // std::cout << std::endl;
-    // mat2.print();
-    // std::cout << std::endl;
-    // mat3.print();
-    // std::cout << std::endl;
-    // mat4.print();
-
-    
-}
-
 // TEST_CASE("test_that_ordered_spin_produces_all_spin_up")
 void test_that_ordered_spin_produces_all_spin_up()
 {
@@ -70,7 +45,6 @@ void test_indexing_boundary_check()
 int main()
 {
     // test_print();
-    test_if_energy_for_four_given_configurations_match_analytic_answer_for_dim_2();
 
     return 0;
 }
--- project4/src/test_energy_solver.cpp
+++ project4/src/test_energy_solver.cpp
@@ -1,1 +1,34 @@
-#include "circular_matrix.h"
+#include "energy_solver.cpp"
+
+// TEST_CASE("test_if_energy_for_four_given_configurations_match_analytic_answer_for_dim_2")
+void test_if_energy_for_four_given_configurations_match_analytic_answer_for_dim_2()
+{
+    double init1[4] = {1, 1, 1, 1};
+    double init2[4] = {1, -1, -1, 1};
+    double init3[4] = {1, 1, -1, 1};
+    double init4[4] = {-1, -1, -1, -1};
+
+    // mat(dimension, configuration)
+    CircularMatrix mat1(2, init1);
+    CircularMatrix mat2(2, init2);
+    CircularMatrix mat3(2, init3);
+    CircularMatrix mat4(2, init4);
+
+    // mat1.print();
+    // std::cout << std::endl;
+    // mat2.print();
+    // std::cout << std::endl;
+    // mat3.print();
+    // std::cout << std::endl;
+    // mat4.print();
+
+    
+}
+
+int main()
+{
+    // test_print();
+    test_if_energy_for_four_given_configurations_match_analytic_answer_for_dim_2();
+
+    return 0;
+}
