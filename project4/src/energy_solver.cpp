497f28dc368ac51b6de4a2f3362de30e5666cac9
Johan <johanafl@student.matnat.uio.no>
2019-11-07 16:47:43 +0100

    Parallel not working yet.

--- project4/src/energy_solver.cpp
+++ project4/src/energy_solver.cpp
@@ -185,6 +185,54 @@ IsingModel::IsingModel(int spin_mat_dim, int mc_iterations_input, long seed) : u
     ising_model_data.open("data_files/ising_model_data.txt", std::ios_base::app);
 }
 
+
+void IsingModel::iterate_temperature_parallel(double initial_temp, double final_temp,
+    int num_of_temp_divided_by_num_of_threds)
+{   /*
+    ????????????????
+    */
+    MPI_Init(NULL, NULL);
+    int world_rank;
+    int world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    double diff_temp = (final_temp - initial_temp)/((world_size + 1)*num_of_temp_divided_by_num_of_threds);
+    double init_temp = initial_temp + diff_temp*num_of_temp_divided_by_num_of_threds*world_rank;
+    double fin_temp = initial_temp + diff_temp*num_of_temp_divided_by_num_of_threds*(world_rank + 1);
+
+    double* sum_total_energy_array = new double[num_of_temp_divided_by_num_of_threds];
+    double* sum_total_energy_squared_array = new double[num_of_temp_divided_by_num_of_threds];
+    double* sum_total_magnetization_array = new double[num_of_temp_divided_by_num_of_threds];
+    double* sum_total_magnetization_absolute_array = new double[num_of_temp_divided_by_num_of_threds];
+    double* sum_total_magnetization_squared_array = new double[num_of_temp_divided_by_num_of_threds];
+
+    double temp;
+
+    for (int num_iteration = 0; num_iteration <= num_of_temp_divided_by_num_of_threds; num_iteration++)
+    {   // looping over temperature values
+
+        temp = init_temp + diff_temp*num_iteration;
+        // pre-calculated exponential values
+        exp_delta_energy[0]  = std::exp(8*J/temp);
+        exp_delta_energy[4]  = std::exp(4*J/temp);
+        exp_delta_energy[8]  = 1;
+        exp_delta_energy[12] = std::exp(-4*J/temp);
+        exp_delta_energy[16] = std::exp(-8*J/temp);
+
+        mc_iteration_stable(temp);
+
+        sum_total_energy_array[world_rank*num_iteration] = sum_total_energy;
+        sum_total_energy_squared_array[world_rank*num_iteration] = sum_total_energy_squared;
+        sum_total_magnetization_array[world_rank*num_iteration] = sum_total_magnetization;
+        sum_total_magnetization_absolute_array[world_rank*num_iteration] = sum_total_magnetization_absolute;
+        sum_total_magnetization_squared_array[world_rank*num_iteration] = sum_total_magnetization_squared;
+
+    }
+
+    MPI_Finalize();
+}
+
+
 void IsingModel::iterate_temperature(double initial_temp, double final_temp,
     double dtemp, bool convergence)
 {   /*
