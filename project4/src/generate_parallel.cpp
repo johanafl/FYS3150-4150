0badc5202c7f4fb13eeb3819ba28108c44158176
Jon Dahl <jonkd@student.matnat.uio.no>
2019-11-07 20:03:06 +0100

    YYYYEEEEEEAAAAHHHH!! Parallel stuff works. Writes to file and all that business.

--- project4/src/generate_parallel.cpp
+++ project4/src/generate_parallel.cpp
@@ -19,7 +19,8 @@ public:
         int world_size;
         MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
         MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-        double diff_temp = (final_temp - initial_temp)/((world_size + 1)*num_of_temp_divided_by_num_of_threds);
+        
+        double diff_temp = (final_temp - initial_temp)/(world_size*num_of_temp_divided_by_num_of_threds);
         double init_temp = initial_temp + diff_temp*num_of_temp_divided_by_num_of_threds*world_rank;
         double fin_temp = initial_temp + diff_temp*num_of_temp_divided_by_num_of_threds*(world_rank + 1);
 
@@ -31,7 +32,7 @@ public:
 
         double temp;
 
-        for (int num_iteration = 0; num_iteration <= num_of_temp_divided_by_num_of_threds; num_iteration++)
+        for (int num_iteration = 0; num_iteration < num_of_temp_divided_by_num_of_threds; num_iteration++)
         {   // looping over temperature values
 
             temp = init_temp + diff_temp*num_iteration;
@@ -44,12 +45,52 @@ public:
 
             mc_iteration_stable(temp);
 
-            sum_total_energy_array[world_rank*num_iteration] = sum_total_energy;
-            sum_total_energy_squared_array[world_rank*num_iteration] = sum_total_energy_squared;
-            sum_total_magnetization_array[world_rank*num_iteration] = sum_total_magnetization;
-            sum_total_magnetization_absolute_array[world_rank*num_iteration] = sum_total_magnetization_absolute;
-            sum_total_magnetization_squared_array[world_rank*num_iteration] = sum_total_magnetization_squared;
+            sum_total_energy_array[num_iteration] = sum_total_energy;
+            sum_total_energy_squared_array[num_iteration] = sum_total_energy_squared;
+            sum_total_magnetization_array[num_iteration] = sum_total_magnetization;
+            sum_total_magnetization_absolute_array[num_iteration] = sum_total_magnetization_absolute;
+            sum_total_magnetization_squared_array[num_iteration] = sum_total_magnetization_squared;
         }
+
+        if (world_rank == 0)
+        {
+            ising_model_data << "mc_iterations: " << mc_iterations;
+            ising_model_data << " spin_matrix_dim: " << n;
+            ising_model_data << std::endl;
+            ising_model_data << std::setw(20) << "T";
+            ising_model_data << std::setw(20) << "<E>";
+            ising_model_data << std::setw(20) << "<E**2>";
+            ising_model_data << std::setw(20) << "<M>";
+            ising_model_data << std::setw(20) << "<M**2>";
+            ising_model_data << std::setw(20) << "<|M|>";
+            ising_model_data << std::endl;
+        }
+
+        for (int rank = 0; rank < world_size; rank++)
+        {
+            if (rank == world_rank)
+            {   
+                for (int i = 0; i < num_of_temp_divided_by_num_of_threds; i++)
+                {
+                    ising_model_data << std::setw(20) << std::setprecision(15) << init_temp + diff_temp*i;
+                    ising_model_data << std::setw(20) << std::setprecision(15) << sum_total_energy_array[i];
+                    ising_model_data << std::setw(20) << std::setprecision(15) << sum_total_energy_squared_array[i];
+                    ising_model_data << std::setw(20) << std::setprecision(15) << sum_total_magnetization_array[i];
+                    ising_model_data << std::setw(20) << std::setprecision(15) << sum_total_magnetization_squared_array[i];
+                    ising_model_data << std::setw(20) << std::setprecision(15) << sum_total_magnetization_absolute_array[i];
+                    ising_model_data << std::endl;
+                }
+            }
+
+            MPI_Barrier(MPI_COMM_WORLD);
+        }
+
+        delete[] sum_total_energy_array;
+        delete[] sum_total_energy_squared_array;
+        delete[] sum_total_magnetization_array;
+        delete[] sum_total_magnetization_absolute_array;
+        delete[] sum_total_magnetization_squared_array;
+        
         MPI_Finalize();
     }
 };
@@ -58,17 +99,17 @@ public:
 int main()
 {   
     int spin_matrix_dim = 2;
-    int mc_iterations = 1e3;
+    int mc_iterations = 1e7;
     
-    double initial_temp = 0.5;
-    double final_temp = 2;
-    double num_of_points = 10;
+    double initial_temp = 1;
+    double final_temp = 3;
+    double num_of_temperatures = 5;
 
     time_t seed;
     time(&seed);
     
     ParallelEnergySolver convergence_model(spin_matrix_dim, mc_iterations, seed);
-    convergence_model.iterate_temperature_parallel(initial_temp, final_temp, num_of_points);
+    convergence_model.iterate_temperature_parallel(initial_temp, final_temp, num_of_temperatures);
 
     return 0;
 }
