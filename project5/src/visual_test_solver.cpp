99dd10c783f8f9bc2da34cfb75f5b1ec9143cdab
Jon Dahl <jonkd@student.matnat.uio.no>
2019-11-28 18:18:55 +0100

    Temporary testing business.

--- project5/src/visual_test_solver.cpp
+++ project5/src/visual_test_solver.cpp
@@ -1,5 +1,35 @@
 #include "solver.h"
 
+
+arma::vec acceleration(arma::vec u, double t)
+{   
+    arma::vec value(3);
+    value.zeros();
+    double r;
+    double x;
+    double y;
+    double z;
+
+    x = u(0);
+    y = u(1);
+    z = u(2);
+    r = std::sqrt(x*x + y*y + z*z);
+
+    value(0) -= 4*pi*pi*x/(r*r*r);
+    value(1) -= 4*pi*pi*y/(r*r*r);
+    value(2) -= 4*pi*pi*z/(r*r*r);
+
+    // a_x = -4*pi*pi*pos_x[i]/(r*r*r);
+    // a_y = -4*pi*pi*pos_y[i]/(r*r*r);
+
+    return value;
+}
+
+arma::vec f(arma::vec u, double t)
+{
+    return arma::zeros<arma::vec>(2);
+}
+
 int main()
 {
     int num_steps_input = 1e5;
@@ -10,10 +40,15 @@ int main()
     double init_vel_x = 0;
     double init_vel_y = 2*pi;
     
-    Solver system(num_steps_input, dt_input, init_pos_x, init_pos_y, init_vel_x, init_vel_y);
     // system.forward_euler();
-    system.velocity_verlet();
-    system.write_to_file();
-    
+    // // system.velocity_verlet();
+    // system.write_to_file();
+
+    ForwardEuler lol(num_steps_input);
+    lol.set_initial_conditions(init_pos_x, init_pos_y, init_vel_x, init_vel_y);
+    lol.solve(acceleration, dt_input);
+    lol.write_to_file();
+
+
     return 0;
 }
