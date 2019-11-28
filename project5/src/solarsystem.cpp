381271301a61a32127ec43f2d054a5100b0ae0b1
Johan <johanafl@student.matnat.uio.no>
2019-11-28 16:04:25 +0100

    Using arma vectors.

--- project5/src/solarsystem.cpp
+++ project5/src/solarsystem.cpp
@@ -1,49 +1,104 @@
 #include "solver.h"
 #include "planet.h"
+#include <armadillo>
 
-double mass_earth   = 6e24;     // [kg]
-double mass_jupiter = 1.9e27;   // [kg]
-double mass_mars    = 6.6e23;   // [kg]
-double mass_venus   = 4.9e24;   // [kg]
-double mass_saturn  = 5.5e26;   // [kg]
-double mass_mercury = 3.3e23;   // [kg]
-double mass_uranus  = 8.8e25;   // [kg]
-double mass_neptun  = 1.03e26;  // [kg]
-double mass_pluto   = 1.31e22;  // [kg]
-
-double dist_earth   = 1;       // [AU]
-double dist_jupiter = 5.20;    // [AU]
-double dist_mars    = 1.52;    // [AU]
-double dist_venus   = 0.72;    // [AU]
-double dist_saturn  = 9.54;    // [AU]
-double dist_mercury = 0.39;    // [AU]
-double dist_uranus  = 19.19;   // [AU]
-double dist_neptun  = 30.06;   // [AU]
-double dist_pluto   = 39.53;   // [AU]
-
-void acceleration()
+class Solarsystem
 {
-    // a = 0
-    // for planet in planets:
-    //      a += G*planet.mass/|planet.r - current_planet.r|^2
-    // return a
-}
+private:
+    
+    int num_planets = 0;
+    int array_size  = 2;
+    Planet* planets = new Planet[array_size];
+
+    // double* ax = new double[array_size];
+    // double* ay = new double[array_size];
+    // double* az = new double[array_size];
+
+    arma::vec value = arma::zeros<arma::vec>(array_size);
+
+    Solarsystem() {}
+
+    void add_planet(double mass, double pos_x, double pos_y, double pos_z, double vel_x, double vel_y, double vel_z)
+    {
+        Planet new_planet(mass, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z);
+        
+        // resize
+        if (num_planets == array_size)
+        {
+            array_size *= 2;
+            Planet* tmp = new Planet[array_size];
+
+            // double* tmp_ax = new double[array_size];
+            // double* tmp_ay = new double[array_size];
+            // double* tmp_az = new double[array_size];
+
+            for (int i=0; i<num_planets; i++)
+            {
+                tmp[i] = planets[i];
+            }
+
+            delete[] planets;
+            // delete[] ax;
+            // delete[] ay;
+            // delete[] az;
+
+            planets = tmp;
+
+            // ax = tmp_ax;
+            // ay = tmp_ay;
+            // az = tmp_az;
+        }
+
+        planets[num_planets] = new_planet;
+        num_planets += 1;
+        value = arma::zeros<arma::vec>(num_planets);
+    }
+
+    arma::vec acceleration(arma::vec u, double t)
+    // acceleration(double u, double t)
+    {
+        double G = 1;
+        double r;
+        double x;
+        double y;
+        double z;
+
+        // arma::vec<double> value[];
+
+        // for planet in planets:
+        for (int i=0; i<num_planets; i++)
+        {
+            for (int j=0; j<num_planets; j++)
+            {
+                x = u(3*j) - u(3*i);
+                y = u(3*j + 1) - u(3*i + 1);
+                z = u(3*j + 2) - u(3*i + 2);
+                r = std::sqrt(x*x + y*y + z*z);
+                // unsure of how to get the sign correct for x, y and z.
+
+                value(3*i)     -= G*planets[j].mass*x/(r*r*r);
+                value(3*i + 1) -= G*planets[j].mass*y/(r*r*r);
+                value(3*i + 2) -= G*planets[j].mass*z/(r*r*r);
+
+                // // a += G*planet.mass/|planet.r - current_planet.r|^2
+                // // Planet MUST contain position to make this work!
+                // r = (planet[i].r - planet[j].r);
+                // x = (planet[i].x - planet[j].x);
+                // y = (planet[i].y - planet[j].y);
+                // z = (planet[i].z - planet[j].z);
+
+                // ax[j] -= G*planets[i].mass*x/(r*r*r);
+                // ay[j] -= G*planets[i].mass*y/(r*r*r);
+                // az[j] -= G*planets[i].mass*z/(r*r*r);
+            }
+        }
+        return value;
+    }
+
+    
+};
 
 int main()
 {
-    int num_planets = 2;
-    Planet* planets = new Planet[num_planets];
-    Planet earth(mass_earth, dist_earth, 0, 0, 0, 2*pi, 0);
-    planets[0] = earth;
-    
-    // std::cout << earth.mass << std::endl;
-    // std::cout << earth.init_x << std::endl;
-    // std::cout << earth.init_y << std::endl;
-    // std::cout << earth.init_z << std::endl;
-    // std::cout << earth.init_vx << std::endl;
-    // std::cout << earth.init_vy << std::endl;
-    // std::cout << earth.init_vz << std::endl;
-    Solver sovle_system(1e5, 1e-2, earth.init_x, earth.init_y, earth.init_vx, earth.init_vy);
-    sovle_system.velocity_verlet();
     return 0;
 }
