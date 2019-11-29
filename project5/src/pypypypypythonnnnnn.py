982a54c13f9526f7348a5f4f030da794f565daba
Jon Dahl <jonkd@student.matnat.uio.no>
2019-11-29 17:39:34 +0100

    Minor syntax.

--- project5/src/pypypypypythonnnnnn.py
+++ project5/src/pypypypypythonnnnnn.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     data = np.loadtxt("data_files/tull_mc_tull.txt", unpack=True)
-    plt.plot(data[1],data[2])
+    plt.plot(data[1], data[2])
     plt.show()
