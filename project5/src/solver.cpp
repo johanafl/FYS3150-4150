1e823c3221225d1d4eb14e8a48bd0fddd0b1d98d
Jon Dahl <jonkd@student.matnat.uio.no>
2019-11-29 13:26:38 +0100

    Velocity Verlet implemented and tested ok. Both integration methods are implemented with arma vectors.

--- project5/src/solver.cpp
+++ project5/src/solver.cpp
@@ -6,6 +6,7 @@ Solver::Solver(int num_steps_input)
     num_steps = num_steps_input;
 }
 
+
 void Solver::set_initial_conditions(double init_pos_x, double init_pos_y,
         double init_vel_x, double init_vel_y)
 {
@@ -25,6 +26,7 @@ void Solver::set_initial_conditions(double init_pos_x, double init_pos_y,
     
 }
 
+
 void Solver::solve(arma::vec (*f)(arma::vec u, double t), double dt_input)
 {
     dt = dt_input;
@@ -36,6 +38,7 @@ void Solver::solve(arma::vec (*f)(arma::vec u, double t), double dt_input)
     }
 }
 
+
 void Solver::advance(arma::vec (*f)(arma::vec u, double t), int k)
 {
     // Dummy method.
@@ -43,58 +46,6 @@ void Solver::advance(arma::vec (*f)(arma::vec u, double t), int k)
 }
 
 
-void Solver::one_step_velocity_verlet(double dt, double abs_dist, double pos_x, 
-    double pos_y, double vel_x, double vel_y, double a_x, double a_y, 
-    double& pos_x_new, double& pos_y_new, double& vel_x_new, 
-    double& vel_y_new)
-{
-    // Velocity verlet
-    pos_x_new = pos_x + dt*vel_x + dt*dt/2*a_x;
-    pos_y_new = pos_y + dt*vel_y + dt*dt/2*a_y;
-    // pos_x[i+1] = pos_x[i] + dt*vel_x[i] + dt*dt/2*a_x;
-    // pos_y[i+1] = pos_y[i] + dt*vel_y[i] + dt*dt/2*a_y;
-
-    double a_x_next = -4*pi*pi*pos_x_new/(abs_dist*abs_dist*abs_dist);
-    double a_y_next = -4*pi*pi*pos_y_new/(abs_dist*abs_dist*abs_dist);
-    // a_x_next = -4*pi*pi*pos_x[i+1]/(r*r*r);
-    // a_y_next = -4*pi*pi*pos_y[i+1]/(r*r*r);
-    
-    vel_x_new = vel_x + dt/2*(a_x_next + a_x);
-    vel_y_new = vel_y + dt/2*(a_y_next + a_y);
-    // vel_x[i+1] = vel_x[i] + dt/2*(a_x_next + a_x);
-    // vel_y[i+1] = vel_y[i] + dt/2*(a_y_next + a_y);            
-}
-
-void Solver::velocity_verlet()
-{
-    double a_x;
-    double a_y;
-    double a_x_next;
-    double a_y_next;
-    double r;
-
-    // Verlet
-    for (int i = 0; i<num_steps-1; i++)
-    {
-        // std::cout << std::endl;
-        r = sqrt(pos_x[i]*pos_x[i] + pos_y[i]*pos_y[i]);
-
-        a_x = -4*pi*pi*pos_x[i]/(r*r*r);
-        a_y = -4*pi*pi*pos_y[i]/(r*r*r);
-
-        one_step_velocity_verlet(dt, r, pos_x[i], pos_y[i], vel_x[i], 
-            vel_y[i], a_x, a_y, pos_x[i+1], pos_y[i+1], vel_x[i+1], vel_y[i+1]);
-
-        // pos_x[i+1] = pos_x[i] + dt*vel_x[i] + dt*dt/2*a_x;
-        // pos_y[i+1] = pos_y[i] + dt*vel_y[i] + dt*dt/2*a_y;
-
-        // a_x_next = -4*pi*pi*pos_x[i+1]/(r*r*r);
-        // a_y_next = -4*pi*pi*pos_y[i+1]/(r*r*r);
-        
-        // vel_x[i+1] = vel_x[i] + dt/2*(a_x_next + a_x);
-        // vel_y[i+1] = vel_y[i] + dt/2*(a_y_next + a_y);
-    }
-}
 
 void Solver::write_to_file()
 {
@@ -120,19 +71,48 @@ void Solver::write_to_file()
     tull_mc_tull.close();
 }
 
+
 Solver::~Solver()
 {
 }
 
 
 void ForwardEuler::advance(arma::vec (*f)(arma::vec u, double t), int k)
-{   
+{   /*
+    One step with Forward Euler.
+
+    Parameters
+    ----------
+    f : function pointer
+        The right hand side of the ODE.
+
+    k : int
+        Current step in the integration.
+    */
+
     arma::vec a = f(pos.col(k), 0);
 
-    vel(0, k+1) = vel(0, k) + dt*a(0);
-    vel(1, k+1) = vel(1, k) + dt*a(1);
+    pos.col(k+1) = pos.col(k) + dt*vel.col(k);
+    vel.col(k+1) = vel.col(k) + dt*a;    
+}
 
-    pos(0, k+1) = pos(0, k) + dt*vel(0, k);
-    pos(1, k+1) = pos(1, k) + dt*vel(1, k);
+void VelocityVerlet::advance(arma::vec (*f)(arma::vec u, double t), int k)
+{   /*
+    One step with Velocity Verlet.
+
+    Parameters
+    ----------
+    f : function pointer
+        The right hand side of the ODE.
+
+    k : int
+        Current step in the integration.
+    */
     
+    arma::vec a1 = f(pos.col(k), 0);
+    pos.col(k+1) = pos.col(k) + dt*vel.col(k) + dt*dt/2*a1;
+
+    arma::vec a2 = f(pos.col(k+1), 0);
+    vel.col(k+1) = vel.col(k) + dt/2*(a2 + a1);
+
 }
--- project5/src/solver.h
+++ project5/src/solver.h
@@ -25,25 +25,14 @@ public:
 
     Solver(int num_steps_input);
 
+    // Virtual to allow a method in the superclass to call a method advance
+    // in a subclass.
     virtual void advance(arma::vec (*f)(arma::vec u, double t), int k);
     
     void set_initial_conditions(double init_pos_x, double init_pos_y,
         double init_vel_x, double init_vel_y);
     
-    void one_step_forward_euler(double dt, double pos_x, double pos_y, 
-        double vel_x, double vel_y, double a_x, double a_y, double& pos_x_new, 
-        double& pos_y_new, double& vel_x_new, double& vel_y_new);
-    
     void solve(arma::vec (*f)(arma::vec u, double t), double dt_input);
-    
-    void forward_euler();
-    
-    void one_step_velocity_verlet(double dt, double abs_dist, double pos_x, 
-        double pos_y, double vel_x, double vel_y, double a_x, double a_y, 
-        double& pos_x_new, double& pos_y_new, double& vel_x_new, 
-        double& vel_y_new);
-    
-    void velocity_verlet();
     
     void write_to_file();
     
@@ -53,7 +42,14 @@ public:
 class ForwardEuler : public Solver
 {
 public:
-    using Solver::Solver;
+    using Solver::Solver;   // For inheriting the constructor of Solver.
+    void advance(arma::vec (*f)(arma::vec u, double t), int k);
+};
+
+class VelocityVerlet : public Solver
+{
+public:
+    using Solver::Solver;   // For inheriting the constructor of Solver.
     void advance(arma::vec (*f)(arma::vec u, double t), int k);
 };
 
