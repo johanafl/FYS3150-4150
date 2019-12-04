e6474dedcd7f13f75ecb39518a9054975d2a1013
Jon Dahl <jonkd@student.matnat.uio.no>
2019-12-05 00:59:38 +0100

    Added options for all planets, but not tested.

--- project5/src/mainfile.cpp
+++ project5/src/mainfile.cpp
@@ -12,7 +12,7 @@ arma::mat fetch_initial_parameters_from_file()
     stellar_init : arma::mat
         Matrix of initial parameters for all planets (and Pluto).
     */
-   
+
     int num_input_params = 6;   // Number of input paramerers per stellar object.
 
     std::string line;
@@ -27,6 +27,7 @@ arma::mat fetch_initial_parameters_from_file()
     
     for (int line_number = 0; line_number < 10; line_number++)
     {   // Iterate over each line in the file.
+        
         std::getline(planet_data, line);
         
         for (int param = 0; param < num_input_params; param++)
@@ -38,6 +39,7 @@ arma::mat fetch_initial_parameters_from_file()
                 word += line[i];
             }
             
+            // Convert the word to double and insert into matrix.
             stellar_init(param, line_number) = std::stod(word);
             word.clear();
 
@@ -55,24 +57,43 @@ arma::mat fetch_initial_parameters_from_file()
 
 int main()
 {   
-    fetch_initial_parameters_from_file();
+    arma::mat all_planets_initial = fetch_initial_parameters_from_file();
 
-    // // U = {rx, ry, rz, vx, vy, vz}
-    // // arma::vec U1 = {1, 0, 0, 0, 2*pi, 0};   // Earth
-    // arma::vec U1 = fetch_initial_parameters_from_file();
-    // arma::vec U2 = {0, 5.2, 0, 2.75, 0, 0}; // Jupiter
-    // double mass1 = 5.972e24; // Earth mass.
-    // double mass2 = 1.898e27; // Jupiter mass.
-    // Solarsystem q;
-    // q.add_planet(mass1, U1);
-    // q.add_planet(mass2, U2);
+    arma::vec sun_initial     = all_planets_initial.col(0);
+    arma::vec mercury_initial = all_planets_initial.col(1);
+    arma::vec venus_initial   = all_planets_initial.col(2);
+    arma::vec earth_initial   = all_planets_initial.col(3);
+    arma::vec mars_initial    = all_planets_initial.col(4);
+    arma::vec jupiter_initial = all_planets_initial.col(5);
+    arma::vec saturn_initial  = all_planets_initial.col(6);
+    arma::vec uranus_initial  = all_planets_initial.col(7);
+    arma::vec neptune_initial = all_planets_initial.col(8);
+    arma::vec pluto_initial   = all_planets_initial.col(9);
+    
+    double sun_mass     = 1.9891e30;
+    double mercury_mass = 3.285e23;
+    double venus_mass   = 4.867e24;
+    double earth_mass   = 5.972e24;
+    double mars_mass    = 6.39e23;
+    double jupiter_mass = 1.898e27;
+    double saturn_mass  = 5.683e26;
+    double uranus_mass  = 8.681e25;
+    double neptune_mass = 1.024e26;
+    double pluto_mass   = 1.309e22;
+    
+    Solarsystem q;
+    // q.add_planet(sun_mass, sun_initial);
+    // q.add_planet(mercury_mass, mercury_initial);
+    // q.add_planet(venus_mass, venus_initial);
+    q.add_planet(earth_mass, earth_initial);
+    // q.add_planet(mars_mass, mars_initial);
+    q.add_planet(jupiter_mass, jupiter_initial);
+    // q.add_planet(saturn_mass, saturn_initial);
+    // q.add_planet(uranus_mass, uranus_initial);
+    // q.add_planet(neptune_mass, neptune_initial);
+    // q.add_planet(pluto_mass, pluto_initial);
 
-    // // arma::vec u0 = q.get_U0();
-    // // u0.print();
-    // q.solve_system();
-    // // arma::vec initial = q.get_U0();
-
-
+    q.solve_system();
 
     return 0;
 }
