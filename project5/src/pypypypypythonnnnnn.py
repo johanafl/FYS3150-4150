b73bcc3a52768ea7a271e766c6b9e4f34da0adc1
Jon Dahl <jonkd@student.matnat.uio.no>
2019-12-05 15:17:57 +0100

    Added all planets.

--- project5/src/pypypypypythonnnnnn.py
+++ project5/src/pypypypypythonnnnnn.py
@@ -3,8 +3,26 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    data = np.loadtxt("data_files/tull_mc_tull.txt", unpack=True)
-    plt.plot(data[1], data[2], label="earth")
-    plt.plot(data[7], data[8], label="jupiter")
+    data = np.loadtxt("data_files/solver_data.txt", unpack=True)
+
+    planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn",
+        "Uranus", "Neptune", "Pluto"]
+    for i in range(9):
+        plt.plot(data[1 + i*6], data[2 + i*6], label=planets[i])
+    
+    # plt.plot(data[1], data[2], label="mercury")
+    # plt.plot(data[1], data[2], label="venus")
+    # plt.plot(data[1], data[2], label="earth")
+    # plt.plot(data[1], data[2], label="mars")
+    # plt.plot(data[1], data[2], label="jupiter")
+    # plt.plot(data[1], data[2], label="saturn")
+    # plt.plot(data[1], data[2], label="uranus")
+    # plt.plot(data[1], data[2], label="neptune")
+    # plt.plot(data[1], data[2], label="pluto")
+
+    # plt.plot(data[1], data[2], label="earth")
+    # plt.plot(data[7], data[8], label="jupiter")
+    # plt.plot(data[13], data[14], label="uranus")
     plt.legend(loc="best")
+    plt.axis("equal")
     plt.show()
